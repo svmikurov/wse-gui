15b043ce1c7f0040bcddebe4e10350eda587d1b8
Sergei Mikurov <454004@mail.ru>
2025-01-04 03:14:56 +0500

    Rename some param sources

--- src/wse/containers/params.py
+++ src/wse/containers/params.py
@@ -34,30 +34,30 @@ class ParamsSources:
         super().__init__()
 
         # Selection sources
-        self.source_category = SourceSelections(ACCESSORS)
-        self.source_source = SourceSelections(ACCESSORS)
-        self.source_order = SourceSelections(ACCESSORS)
-        self.source_period_start_date = SourceSelections(ACCESSORS)
-        self.source_period_end_date = SourceSelections(ACCESSORS)
+        self.category = SourceSelections(ACCESSORS)
+        self.source = SourceSelections(ACCESSORS)
+        self.order = SourceSelections(ACCESSORS)
+        self.period_start_date = SourceSelections(ACCESSORS)
+        self.period_end_date = SourceSelections(ACCESSORS)
 
         # Decimal sources
-        self.source_input_count_first = SourceDecimal()
-        self.source_input_count_last = SourceDecimal()
-        self.source_timeout = SourceDecimal()
+        self.count_first = SourceDecimal()
+        self.count_last = SourceDecimal()
+        self.timeout = SourceDecimal()
 
         # Switches first or last, timeout
-        self.source_is_first = SourceSwitch()
-        self.source_is_last = SourceSwitch()
-        self.source_has_timeout = SourceSwitch()
+        self.is_first = SourceSwitch()
+        self.is_last = SourceSwitch()
+        self.has_timeout = SourceSwitch()
 
         # Switch progress sources
-        self.source_progress_study = SourceSwitch()
-        self.source_progress_repeat = SourceSwitch()
-        self.source_progress_examination = SourceSwitch()
-        self.source_progress_know = SourceSwitch()
+        self.progress_study = SourceSwitch()
+        self.progress_repeat = SourceSwitch()
+        self.progress_examination = SourceSwitch()
+        self.progress_know = SourceSwitch()
 
         # Switch favorites
-        self.source_favorites = SourceSwitch()
+        self.favorites = SourceSwitch()
 
 
 class ParamsLogic(MessageMixin, ParamsSources):
@@ -108,68 +108,68 @@ class ParamsLogic(MessageMixin, ParamsSources):
     def populate_selections(self) -> None:
         """Populate the selections with the choices."""
         # Only selections has choices.
-        self.source_category.update_data(self.exercise_choices['categories'])
-        self.source_source.update_data(self.exercise_choices['source'])
-        self.source_order.update_data(self.exercise_choices['orders'])
-        self.source_period_start_date.update_data(self.exercise_choices['edge_period_items'])  # noqa: E501
-        self.source_period_end_date.update_data(self.exercise_choices['edge_period_items'])  # noqa: E501
+        self.category.update_data(self.exercise_choices['categories'])
+        self.source.update_data(self.exercise_choices['source'])
+        self.order.update_data(self.exercise_choices['orders'])
+        self.period_start_date.update_data(self.exercise_choices['edge_period_items'])  # noqa: E501
+        self.period_end_date.update_data(self.exercise_choices['edge_period_items'])  # noqa: E501
 
     def set_default_params(self) -> None:
         """Set default params."""
         # Selections
-        self.source_category.set_value(self.default_values['category'])
-        self.source_source.set_value(self.default_values['source'])
-        self.source_order.set_value(self.default_values['order'])
-        self.source_period_start_date.set_value(self.default_values['period_start_date'])  # noqa: E501
-        self.source_period_end_date.set_value(self.default_values['period_end_date'])  # noqa: E501
+        self.category.set_value(self.default_values['category'])
+        self.source.set_value(self.default_values['source'])
+        self.order.set_value(self.default_values['order'])
+        self.period_start_date.set_value(self.default_values['period_start_date'])  # noqa: E501
+        self.period_end_date.set_value(self.default_values['period_end_date'])
         # Inputs
-        self.source_is_first.set_value(self.default_values['is_first'])
-        self.source_is_last.set_value(self.default_values['is_last'])
-        self.source_input_count_first.set_value(self.default_values['count_first'])  # noqa: E501
-        self.source_input_count_last.set_value(self.default_values['count_last'])  # noqa: E501
-        self.source_has_timeout.set_value(self.default_values['has_timeout'])
-        self.source_timeout.set_value(self.default_values['timeout'])
+        self.is_first.set_value(self.default_values['is_first'])
+        self.is_last.set_value(self.default_values['is_last'])
+        self.count_first.set_value(self.default_values['count_first'])
+        self.count_last.set_value(self.default_values['count_last'])
+        self.has_timeout.set_value(self.default_values['has_timeout'])
+        self.timeout.set_value(self.default_values['timeout'])
         # Switches
-        self.source_progress_study.set_value('S' in self.default_values['progress'])  # noqa: E501
-        self.source_progress_repeat.set_value('R' in self.default_values['progress'])  # noqa: E501
-        self.source_progress_examination.set_value('E' in self.default_values['progress'])  # noqa: E501
-        self.source_progress_know.set_value('K' in self.default_values['progress'])  # noqa: E501
-        self.source_favorites.set_value(self.default_values['favorites'])  # noqa: E501
+        self.progress_study.set_value('S' in self.default_values['progress'])
+        self.progress_repeat.set_value('R' in self.default_values['progress'])
+        self.progress_examination.set_value('E' in self.default_values['progress'])  # noqa: E501
+        self.progress_know.set_value('K' in self.default_values['progress'])
+        self.favorites.set_value(self.default_values['favorites'])
 
     def set_saved_params(self) -> None:
         """Set saved params."""
         # Selections
-        self.source_category.set_value(self.lookup_conditions['category'])
-        self.source_source.set_value(self.lookup_conditions['source'])
-        self.source_order.set_value(self.lookup_conditions['order'])
-        self.source_period_start_date.set_value(self.lookup_conditions['period_start_date'])  # noqa: E501
-        self.source_period_end_date.set_value(self.lookup_conditions['period_end_date'])  # noqa: E501
+        self.category.set_value(self.lookup_conditions['category'])
+        self.source.set_value(self.lookup_conditions['source'])
+        self.order.set_value(self.lookup_conditions['order'])
+        self.period_start_date.set_value(self.lookup_conditions['period_start_date'])  # noqa: E501
+        self.period_end_date.set_value(self.lookup_conditions['period_end_date'])  # noqa: E501
         # Inputs
-        self.source_is_first.set_value(self.lookup_conditions['is_first'])
-        self.source_is_last.set_value(self.lookup_conditions['is_last'])
-        self.source_input_count_first.set_value(self.lookup_conditions['count_first'])  # noqa: E501
-        self.source_input_count_last.set_value(self.lookup_conditions['count_last'])  # noqa: E501
-        self.source_has_timeout.set_value(self.lookup_conditions['has_timeout'])
-        self.source_timeout.set_value(self.lookup_conditions['timeout'])
+        self.is_first.set_value(self.lookup_conditions['is_first'])
+        self.is_last.set_value(self.lookup_conditions['is_last'])
+        self.count_first.set_value(self.lookup_conditions['count_first'])
+        self.count_last.set_value(self.lookup_conditions['count_last'])
+        self.has_timeout.set_value(self.lookup_conditions['has_timeout'])
+        self.timeout.set_value(self.lookup_conditions['timeout'])
 
         # Switches
-        self.source_progress_study.set_value('S' in self.lookup_conditions['progress'])  # noqa: E501
-        self.source_progress_repeat.set_value('R' in self.lookup_conditions['progress'])  # noqa: E501
-        self.source_progress_examination.set_value('E' in self.lookup_conditions['progress'])  # noqa: E501
-        self.source_progress_know.set_value('K' in self.lookup_conditions['progress'])  # noqa: E501
-        self.source_favorites.set_value(self.lookup_conditions['favorites'])  # noqa: E501
+        self.progress_study.set_value('S' in self.lookup_conditions['progress'])  # noqa: E501
+        self.progress_repeat.set_value('R' in self.lookup_conditions['progress'])  # noqa: E501
+        self.progress_examination.set_value('E' in self.lookup_conditions['progress'])  # noqa: E501
+        self.progress_know.set_value('K' in self.lookup_conditions['progress'])
+        self.favorites.set_value(self.lookup_conditions['favorites'])
     # fmt: on
 
     def get_progress_choice(self) -> list:
         """Get progress choice using switches."""
         progress = []
-        if self.source_progress_study.get_value():
+        if self.progress_study.get_value():
             progress.append('S')
-        if self.source_progress_repeat.get_value():
+        if self.progress_repeat.get_value():
             progress.append('R')
-        if self.source_progress_examination.get_value():
+        if self.progress_examination.get_value():
             progress.append('E')
-        if self.source_progress_know.get_value():
+        if self.progress_know.get_value():
             progress.append('K')
         return progress
 
@@ -185,19 +185,19 @@ class ParamsLogic(MessageMixin, ParamsSources):
     async def request_save_lookup_conditions(self) -> None:
         """Request to save user lookup conditions."""
         lookup_conditions = {
-            'category': self.source_category.value.alias,
-            'source': self.source_source.value.alias,
-            'count_first': self.source_input_count_first.get_value(),
-            'count_last': self.source_input_count_last.get_value(),
-            'favorites': self.source_favorites.get_value(),
-            'is_first': self.source_is_first.get_value(),
-            'is_last': self.source_is_last.get_value(),
-            'order': self.source_order.value.alias,
-            'period_start_date': self.source_period_start_date.value.alias,
-            'period_end_date': self.source_period_end_date.value.alias,
+            'category': self.category.value.alias,
+            'source': self.source.value.alias,
+            'count_first': self.count_first.get_value(),
+            'count_last': self.count_last.get_value(),
+            'favorites': self.favorites.get_value(),
+            'is_first': self.is_first.get_value(),
+            'is_last': self.is_last.get_value(),
+            'order': self.order.value.alias,
+            'period_start_date': self.period_start_date.value.alias,
+            'period_end_date': self.period_end_date.value.alias,
             'progress': self.get_progress_choice(),
-            'has_timeout': self.source_has_timeout.get_value(),
-            'timeout': self.source_timeout.get_value(),
+            'has_timeout': self.has_timeout.get_value(),
+            'timeout': self.timeout.get_value(),
         }
         await request_put_async(url=self.url, payload=lookup_conditions)
 
@@ -218,45 +218,45 @@ class ParamsWidgets(HttpPutMixin, WidgetMixin, ParamsLogic):
 
         # fmt: off
         # Selections
-        self.selection_category = Selection(accessor='name', items=self.source_category)  # noqa: E501
-        self.selection_source = Selection(accessor='name', items=self.source_source)  # noqa: E501
-        self.selection_order = Selection(accessor='name', items=self.source_order)  # noqa: E501
-        self.selection_period_start_date = Selection(accessor='name', items=self.source_period_start_date)  # noqa: E501
-        self.selection_period_end_date = Selection(accessor='name', items=self.source_period_end_date)  # noqa: E501
+        self.selection_category = Selection(accessor='name', items=self.category)  # noqa: E501
+        self.selection_source = Selection(accessor='name', items=self.source)  # noqa: E501
+        self.selection_order = Selection(accessor='name', items=self.order)  # noqa: E501
+        self.selection_period_start_date = Selection(accessor='name', items=self.period_start_date)  # noqa: E501
+        self.selection_period_end_date = Selection(accessor='name', items=self.period_end_date)  # noqa: E501
 
         # Switches of count
-        self.switch_is_first = SwitchApp(text='', on_change=self.source_is_first.update_value)  # noqa: E501
-        self.switch_is_last = SwitchApp(text='', on_change=self.source_is_last.update_value)  # noqa: E501
+        self.switch_is_first = SwitchApp(text='', on_change=self.is_first.update_value)  # noqa: E501
+        self.switch_is_last = SwitchApp(text='', on_change=self.is_last.update_value)  # noqa: E501
         # Switches are listeners.
-        self.source_is_first.add_listener(self.switch_is_first)
-        self.source_is_last.add_listener(self.switch_is_last)
+        self.is_first.add_listener(self.switch_is_first)
+        self.is_last.add_listener(self.switch_is_last)
         # NumberInputs
-        self.input_count_first = NumberInputApp(step=10, min=0, on_change=self.source_input_count_first.update_value)  # noqa: E501
-        self.input_count_last = NumberInputApp(step=10, min=0, on_change=self.source_input_count_last.update_value)  # noqa: E501
+        self.input_count_first = NumberInputApp(step=10, min=0, on_change=self.count_first.update_value)  # noqa: E501
+        self.input_count_last = NumberInputApp(step=10, min=0, on_change=self.count_last.update_value)  # noqa: E501
         # NumberInputs ara listeners.
-        self.source_input_count_first.add_listener(self.input_count_first)
-        self.source_input_count_last.add_listener(self.input_count_last)
+        self.count_first.add_listener(self.input_count_first)
+        self.count_last.add_listener(self.input_count_last)
 
         # Switches of progress
-        self.switch_study = SwitchApp(text='', on_change=self.source_progress_study.update_value)  # noqa: E501
-        self.switch_repeat = SwitchApp(text='', on_change=self.source_progress_repeat.update_value)  # noqa: E501
-        self.switch_examination = SwitchApp(text='', on_change=self.source_progress_examination.update_value)  # noqa: E501
-        self.switch_know = SwitchApp(text='', on_change=self.source_progress_know.update_value)  # noqa: E501
+        self.switch_study = SwitchApp(text='', on_change=self.progress_study.update_value)  # noqa: E501
+        self.switch_repeat = SwitchApp(text='', on_change=self.progress_repeat.update_value)  # noqa: E501
+        self.switch_examination = SwitchApp(text='', on_change=self.progress_examination.update_value)  # noqa: E501
+        self.switch_know = SwitchApp(text='', on_change=self.progress_know.update_value)  # noqa: E501
         # Switches are listeners.
-        self.source_progress_study.add_listener(self.switch_study)
-        self.source_progress_repeat.add_listener(self.switch_repeat)
-        self.source_progress_examination.add_listener(self.switch_examination)
-        self.source_progress_know.add_listener(self.switch_know)
+        self.progress_study.add_listener(self.switch_study)
+        self.progress_repeat.add_listener(self.switch_repeat)
+        self.progress_examination.add_listener(self.switch_examination)
+        self.progress_know.add_listener(self.switch_know)
 
         # Switch of favorites
-        self.switch_favorites = SwitchApp(text='', on_change=self.source_favorites.update_value)  # noqa: E501
-        self.source_favorites.add_listener(self.switch_favorites)
+        self.switch_favorites = SwitchApp(text='', on_change=self.favorites.update_value)  # noqa: E501
+        self.favorites.add_listener(self.switch_favorites)
 
         # Timeout
-        self.switch_timeout = SwitchApp(text='', on_change=self.source_has_timeout.update_value)  # noqa: E501
-        self.input_timeout = NumberInputApp(step=1, min=0, on_change=self.source_timeout.update_value)  # noqa: E501
-        self.source_has_timeout.add_listener(self.switch_timeout)
-        self.source_timeout.add_listener(self.input_timeout)
+        self.switch_timeout = SwitchApp(text='', on_change=self.has_timeout.update_value)  # noqa: E501
+        self.input_timeout = NumberInputApp(step=1, min=0, on_change=self.timeout.update_value)  # noqa: E501
+        self.has_timeout.add_listener(self.switch_timeout)
+        self.timeout.add_listener(self.input_timeout)
 
         # Buttons
         self.btn_goto_exercise = BtnApp('Начать упражнение', on_press=self.start_exercise_handler)  # noqa: E501
