5a6a327d1c7a00515096ae48635d1e366ad515fe
Sergei Mikurov <454004@mail.ru>
2025-01-09 01:26:14 +0500

    Refactor exercise controller

--- src/wse/controllers/exercise.py
+++ src/wse/controllers/exercise.py
@@ -1,6 +1,7 @@
 """Exercise controller."""
 
 from http import HTTPStatus
+from pprint import pprint
 from typing import TypeVar
 
 import toga
@@ -24,7 +25,7 @@ class ControllerExercise:
         self.url_progress = None
         self.timer = Timer()
         self._task = Task()
-        self._has_action = False
+        self._has_assessment = False
 
         # Sources
         self.question = Source()
@@ -35,9 +36,16 @@ class ControllerExercise:
     async def on_open(self, _: toga.Widget) -> None:
         """Start exercise when box was assigned to window content."""
         self._set_exercise_params()
-        self._select_widgets()
+        self._select_widgets_to_display()
         self._reset_task_status()
         await self._loop_exercise()
+
+    def _set_exercise_params(self) -> None:
+        """Set lookup conditions of items to use in the exercise."""
+        lookup_conditions = self._app.plc_params.lookup_conditions
+        self.timer.has_timeout = lookup_conditions.pop('has_timeout')
+        self.timer.timeout = lookup_conditions.pop('timeout')
+        self._task.params = lookup_conditions
 
     ####################################################################
     # Loop exercise
@@ -53,11 +61,11 @@ class ControllerExercise:
                 if not self._task.data:
                     break
                 self._show_question()
-                self._task.status = 'answer'
-                self._has_action = False
+                self._change_task_status(next_status='answer')
+                self._reset_assessment_event()
             else:
                 self._show_answer()
-                self._task.status = 'question'
+                self._change_task_status(next_status='question')
 
             if self.timer.has_timeout:
                 await self.timer.start()
@@ -66,10 +74,8 @@ class ControllerExercise:
                 # if the exercise is paused.
                 self.timer.on_pause()
 
-    def _on_loop_exercise(self):
-        """Reset the state to start the loop exercise."""
-        self.timer.cancel()
-        self.timer.unpause()
+    ####################################################################
+    # Notifications
 
     def _show_question(self) -> None:
         """Show the question."""
@@ -86,7 +92,12 @@ class ControllerExercise:
         self.answer.notify('clean')
 
     ####################################################################
-    # Utility methods
+    # Loop exercise nethods
+
+    def _on_loop_exercise(self) -> None:
+        """Reset the state to start the loop exercise."""
+        self.timer.cancel()
+        self.timer.unpause()
 
     def _is_enable_new_task(self) -> bool:
         """Return `False` to cancel task, `True` otherwise."""
@@ -98,16 +109,17 @@ class ControllerExercise:
         """Is the box of widget is main window content."""
         return self._app.main_window.content is self._app.box_foreign_exercise
 
-    def _set_exercise_params(self) -> None:
-        """Set lookup conditions of items to use in the exercise."""
-        lookup_conditions = self._app.plc_params.lookup_conditions
-        self.timer.has_timeout = lookup_conditions.pop('has_timeout')
-        self.timer.timeout = lookup_conditions.pop('timeout')
-        self._task.params = lookup_conditions
-
-    def _reset_task_status(self):
+    def _reset_task_status(self) -> None:
         """Reset the task status."""
         self._task.status = None
+
+    def _change_task_status(self, next_status: str) -> None:
+        """Change the task status."""
+        self._task.status = next_status
+
+    def _reset_assessment_event(self) -> None:
+        """Reset event of item assessment."""
+        self._has_assessment = False
 
     ####################################################################
     # HTTP requests
@@ -117,15 +129,16 @@ class ControllerExercise:
         response = request_post(self.url_exercise, self._task.params)
         if response.status_code == HTTPStatus.OK:
             self._task.data = response.json()
+            pprint(self._task.data)
             return
         # elif response.status_code == HTTPStatus.NO_CONTENT:
         #     # TODO:Add message no task.
         #     await self.move_to_box_params(self)
         self._task.data = None
 
-    async def _update_item_progress(self, action):
+    async def _update_item_progress(self, assessment: str) -> None:
         """Request to update progress study of item."""
-        payload = {'action': action, 'item_id': self._task.item_id}
+        payload = {'action': assessment, 'item_id': self._task.item_id}
         await request_post_async(self.url_progress, payload)
 
     ####################################################################
@@ -137,15 +150,15 @@ class ControllerExercise:
 
     async def not_know(self, _: toga.Widget) -> None:
         """Mark item in question as not know."""
-        if not self._has_action:
-            await self._update_item_progress(action='not_know')
-            self._has_action = True
+        if not self._has_assessment:
+            await self._update_item_progress(assessment='not_know')
+            self._has_assessment = True
         await self._move_to_next_task_status()
 
     async def know(self, _: toga.Widget) -> None:
         """Mark item in question as know."""
-        if not self._has_action:
-            await self._update_item_progress(action='know')
+        if not self._has_assessment:
+            await self._update_item_progress(assessment='know')
         await self._move_to_next_task()
 
     async def next(self, _: toga.Widget) -> None:
@@ -160,13 +173,13 @@ class ControllerExercise:
         self._task.status = None
         await self._move_to_next_task_status()
 
-    async def _move_to_next_task_status(self):
+    async def _move_to_next_task_status(self) -> None:
         """Move to next task status."""
         await self._loop_exercise()
 
     ####################################################################
     # Page events
 
-    def _select_widgets(self) -> None:
+    def _select_widgets_to_display(self) -> None:
         """Select widgets according to exercise parameters."""
         self.event.notify('update_availability_pause_button')
