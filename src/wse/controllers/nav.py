e778e5c2fa1e17af6b00ac3caf2d1cfb64b772ba
Sergei Mikurov <454004@mail.ru>
2025-02-10 00:11:25 +0500

    Refactor navigation controller

--- src/wse/controllers/nav.py
+++ src/wse/controllers/nav.py
@@ -24,56 +24,32 @@ class Navigation:
         """Delete the class instance."""
         Navigation.__instance = None
 
-    @staticmethod
-    def _create_attrs(text: str, on_press: Callable) -> dict:
-        """Return the button named attributes."""
-        return {'text': text, 'on_press': on_press}
-
     ####################################################################
-    # Navigation targets                                               #
-    ####################################################################
+    # Main
 
-    @property
-    def login(self) -> dict:
-        """The attrs of button go to login page."""
-        return self._create_attrs('Вход в учетную запись', gh.goto_login)
+    login = {'text': 'Вход в учетную запись', 'on_press': gh.goto_login}
 
     ####################################################################
     # Foreign
 
-    @property
-    def foreign_main(self) -> dict:
-        """The attrs of button go to foreign main page."""
-        return self._create_attrs('Иностранный', gh.goto_foreign_main)
+    foreign_main = {'text': 'Иностранный', 'on_press': gh.goto_foreign_main}
 
     ####################################################################
     # Glossary
 
-    @property
-    def glossary_main(self) -> dict:
-        """The attrs of button go to glossary main page."""
-        return self._create_attrs('Глоссарий', gh.goto_glossary_main)
+    glossary_main = {'text': 'Глоссарий', 'on_press': gh.goto_glossary_main}
 
     ####################################################################
     # Mathematics
 
-    @property
-    def math_main(self) -> dict:
-        """The attrs of button go to mathematics main page."""
-        return self._create_attrs('Математика', gh.goto_math_main)
+    math_main = {'text': 'Математика', 'on_press': gh.goto_math_main}
 
     ####################################################################
     # Mentoring
 
-    @property
-    def mentoring(self) -> dict:
-        """The attrs of button go to mentoring main page."""
-        return self._create_attrs('Выполнение заданий', gh.goto_mentoring)
+    mentoring = {'text': 'Выполнение заданий', 'on_press': gh.goto_mentoring}
 
     ####################################################################
     # Explorer
 
-    @property
-    def explorer_main(self) -> dict:
-        """The attrs of button go to Toga explorer main page."""
-        return self._create_attrs('Изучение виджетов', gh.goto_explorer)
+    explorer_main = {'text': 'Изучение виджетов', 'on_press': gh.goto_explorer}
