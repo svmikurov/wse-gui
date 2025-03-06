ccc4abb545138b113d45fd4d6df629cee0dfda9c
Sergei Mikurov <454004@mail.ru>
2025-03-06 19:44:25 +0500

    feat(i18n): add dynamic initialization of languages

--- src/wse/utils/i18n.py
+++ src/wse/utils/i18n.py
@@ -1,10 +1,11 @@
 """Manages internationalization and language settings."""
 
 import gettext
-from pathlib import Path
 from typing import Any, Dict, Optional
 
-from wse.core.config import PROJECT_PATH, Language, Settings
+from typing_extensions import Self
+
+from wse.core.config import PROJECT_PATH, Languages, Settings
 from wse.core.logger import setup_logger
 
 logger = setup_logger('language')
@@ -14,35 +15,33 @@ class I18N:
     """Handles translations and language settings."""
 
     _instance: Optional['I18N'] = None
-    locale_dir: Path
     translations: Dict[str, Any]
     current_lang: str
+    locale_dir = PROJECT_PATH / 'src' / 'wse' / 'locales'
 
-    def __new__(cls, settings: Optional[Settings] = None) -> None:
+    def __new__(cls, settings: Optional[Settings] = None) -> Self:
         """Construct the class."""
         if not cls._instance:
             cls._instance = super().__new__(cls)
-            cls._instance.init_translations(settings)
+            cls._init_languages()
+            cls._instance._init_translations(settings)
         return cls._instance
 
-    def init_translations(self, settings: Optional[Settings]) -> None:
+    @classmethod
+    def _init_languages(cls) -> None:
+        """Initialize translations for all supported languages."""
+        cls.translations = {}
+        for language in Languages:
+            cls.translations[language] = gettext.translation(
+                domain='app',
+                localedir=cls.locale_dir,
+                languages=[language],
+                fallback=True,
+            )
+
+    def _init_translations(self, settings: Optional[Settings]) -> None:
         """Initialize translations."""
-        self.locale_dir = PROJECT_PATH / 'src' / 'wse' / 'locales'
-        self.translations = {
-            Language.EN: gettext.translation(
-                'app',
-                self.locale_dir,
-                languages=[Language.EN],
-                fallback=True,
-            ),
-            Language.RU: gettext.translation(
-                'app',
-                self.locale_dir,
-                languages=[Language.RU],
-                fallback=True,
-            ),
-        }
-        self.current_lang = settings.LANGUAGE if settings else Language.EN
+        self.current_lang = settings.LANGUAGE if settings else Languages.EN
         self.translations[self.current_lang].install()
 
     def set_language(self, lang: str) -> None:
@@ -50,13 +49,24 @@ class I18N:
         if lang in self.translations:
             self.current_lang = lang
             self.translations[lang].install()
-            logger.info(f'Установлен язык приложения: {lang}')
+            logger.info(f'The application language is set to: {lang}')
         else:
-            logger.warning(f'Неподдерживаемый язык: {lang}')
+            logger.warning(f'Unsupported language: {lang}')
+
+    @classmethod
+    def get_current_language(cls) -> str:
+        """Get current language."""
+        return cls._instance.current_lang if cls._instance else None
 
     def gettext(self, text: str) -> str:
         """Return the translated text."""
-        return self.translations[self.current_lang].gettext(text)
+        if self.current_lang in self.translations:
+            return self.translations[self.current_lang].gettext(text)
+        else:
+            logger.warning(
+                f'Language {self.current_lang} not found in Translations'
+            )
+            return text
 
 
 # Global copy of translations
