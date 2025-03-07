cb11e3db5696c258b373ae2298f12a76202dd655
Sergei Mikurov <454004@mail.ru>
2025-03-07 17:38:57 +0500

    feat(ui): Add and style login screen widgets

--- src/wse/features/auth/view.py
+++ src/wse/features/auth/view.py
@@ -1,21 +1,73 @@
 """Defines the login screen view."""
 
 import toga
+from toga.constants import CENTER
+from toga.style import Pack
 
-from wse.features.shared.view import BaseView
+from wse.features.shared.ui.box import ColumnFlexBox
 from wse.utils.i18n import _
 
 
-class LoginView(BaseView):
+class LoginView(ColumnFlexBox):
     """Represents the login screen."""
 
-    def __init__(self, *args: object, **kwargs: object) -> None:
-        """Construct the view."""
-        super().__init__(*args, **kwargs)
+    def __init__(self) -> None:
+        """Initialize the view."""
+        super().__init__()
+        self._setup_styles()
+        self._create_widgets()
+        self._add_widgets_to_view()
 
-        self.heading = toga.Label(_('Login'))
+    def _setup_styles(self) -> None:
+        """Define styles for widgets."""
+        self.FONT_SIZE = 20
+        self.HEADING_PADDING_TOP = 30
+        self.HEADING_PADDING_BOTTOM = 10
+        self.INPUT_PADDING_TOP = 10
+        self.SUBMIT_PADDING_TOP = 20
 
-        # DOM
+        self.general_style = {
+            "padding_right": 25,
+            "padding_left": 25,
+            "font_size": self.FONT_SIZE,
+            "height": 70,
+        }
+
+    def _create_widgets(self) -> None:
+        """Create widgets for the login screen."""
+        self.heading = toga.Label(
+            text=_("Login"),
+            style=Pack(
+                padding_top=self.HEADING_PADDING_TOP,
+                padding_bottom=self.HEADING_PADDING_BOTTOM,
+                text_align=CENTER,
+                font_size=self.FONT_SIZE,
+            ),
+        )
+        self.username_input = toga.TextInput(
+            placeholder=_("Username"),
+            style=Pack(
+                padding_top=self.INPUT_PADDING_TOP, **self.general_style
+            ),
+        )
+        self.password_input = toga.PasswordInput(
+            placeholder=_("Password"),
+            style=Pack(
+                padding_top=self.INPUT_PADDING_TOP, **self.general_style
+            ),
+        )
+        self.submit_button = toga.Button(
+            text=_("Login"),
+            style=Pack(
+                padding_top=self.SUBMIT_PADDING_TOP, **self.general_style
+            ),
+        )
+
+    def _add_widgets_to_view(self) -> None:
+        """Add widgets to the view."""
         self.add(
             self.heading,
+            self.username_input,
+            self.password_input,
+            self.submit_button,
         )
