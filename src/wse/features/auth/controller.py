c9317a3d9bbbdcad7e048e4b082b5f005241cec8
Sergei Mikurov <454004@mail.ru>
2025-03-08 14:13:51 +0500

    feat(login controller): Add login controller

--- src/wse/features/auth/controller.py
+++ src/wse/features/auth/controller.py
@@ -1,1 +1,19 @@
 """Manages the logic for the authentication feature."""
+
+from toga.sources import Listener
+
+from wse.features.auth.model import UserModel
+from wse.features.auth.view import LoginView
+
+
+class LoginController(Listener):
+    """Login screen controller."""
+
+    def __init__(self, model: UserModel, view: LoginView) -> None:
+        """Construct the controller."""
+        self.model = model
+        self.view = view
+
+    def handel_login(self) -> None:
+        """Handel the submit login event."""
+        print('Login event')
