4f31e44532e8ab097b8819845beabbeb8dffa5e6
Sergei Mikurov <454004@mail.ru>
2025-03-08 21:28:45 +0500

    feat(main): Add Exercises button to main screen
    - Added a button labeled "Exercises" to the main screen.
    - Refactored view initialization into separate methods:
      _create_widgets and _add_widgets_to_view.

--- src/wse/features/main/view.py
+++ src/wse/features/main/view.py
@@ -1,6 +1,7 @@
 """Defines the main screen view."""
 
 from wse.features.shared.ui.box import ColumnFlexBox
+from wse.features.shared.ui.button import ButtonStyled
 from wse.features.shared.ui.heading import Heading
 from wse.utils.i18n import _
 
@@ -11,10 +12,17 @@ class HomeView(ColumnFlexBox):
     def __init__(self) -> None:
         """Construct the view."""
         super().__init__()
+        self._create_widgets()
+        self._add_widgets_to_view()
 
+    def _create_widgets(self) -> None:
         self.heading = Heading(_('Home'))
+        self.flex_box = ColumnFlexBox()
+        self.back_button = ButtonStyled(_('Exercises'))
 
-        # DOM
+    def _add_widgets_to_view(self) -> None:
         self.add(
             self.heading,
+            self.flex_box,
+            self.back_button,
         )
