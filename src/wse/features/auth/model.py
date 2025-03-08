1bc200c03acfadb1df4813bd0b67c39e3ed4d427
Sergei Mikurov <454004@mail.ru>
2025-03-08 14:12:33 +0500

    feat(user): Add user model

--- src/wse/features/auth/model.py
+++ src/wse/features/auth/model.py
@@ -1,1 +1,11 @@
 """Defines the data model for the authentication feature."""
+
+
+class UserModel:
+    """User model."""
+
+    def __init__(self) -> None:
+        """Construct the model."""
+        super().__init__()
+        self.username: str | None = None
+        self.password: str | None = None
