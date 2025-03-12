9b4185bbd209444f90d6ffef21f6286bb4b3fc5c
Sergei Mikurov <454004@mail.ru>
2025-03-12 09:28:04 +0500

    feat(auth): Add authentication method to UserModel

--- src/wse/features/auth/model.py
+++ src/wse/features/auth/model.py
@@ -1,11 +1,21 @@
 """Defines the data model for the authentication feature."""
 
+from wse.core.auth.auth import AuthService
+from wse.core.logger import setup_logger
+
+logger = setup_logger('features.auth.UserModel')
+
 
 class UserModel:
     """User model."""
 
-    def __init__(self) -> None:
+    def __init__(self, auth_service: AuthService) -> None:
         """Construct the model."""
         super().__init__()
+        self.auth_service = auth_service
         self.username: str | None = None
         self.password: str | None = None
+
+    async def authenticate(self, username: str, password: str) -> None:
+        """Authenticate user."""
+        await self.auth_service.authenticate(username, password)
