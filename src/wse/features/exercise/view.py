8abe63b21841b6951ca394cae39702c12eff4ced
Sergei Mikurov <454004@mail.ru>
2025-03-12 13:57:55 +0500

    refactor(exercises): Update ExercisesView with Subject

--- src/wse/features/exercise/view.py
+++ src/wse/features/exercise/view.py
@@ -6,6 +6,7 @@ from toga.sources import Source
 from wse.features.shared.ui.box import ColumnFlexBox
 from wse.features.shared.ui.button import ButtonStyled
 from wse.features.shared.ui.heading import Heading
+from wse.features.shared.ui.text import MultilineInfoPanel
 from wse.utils.i18n import _
 
 
@@ -18,23 +19,23 @@ class ExercisesView(ColumnFlexBox, Source):
         Source.__init__(self)
         self._create_widgets()
         self._add_widgets_to_view()
-        self._assign_widget_handlers()
 
     def _create_widgets(self) -> None:
         self.heading = Heading(_('Exercises'))
-        self.back_button = ButtonStyled(_('Back'))
+        self.info_panel = MultilineInfoPanel()
+        self.back_button = ButtonStyled(
+            _('Back'), on_press=self._on_back_click
+        )
 
     def _add_widgets_to_view(self) -> None:
         self.add(
             self.heading,
+            self.info_panel,
             self.back_button,
         )
 
     ####################################################################
     # Widget handlers
-
-    def _assign_widget_handlers(self) -> None:
-        self.back_button.on_press = self._on_back_click
 
     def _on_back_click(self, _: toga.Widget) -> None:
         self.notify('back')
