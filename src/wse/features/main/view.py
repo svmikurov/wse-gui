dc7b6cbc6d23a949d5ef999efc1cac01b6fc4ac7
Sergei Mikurov <454004@mail.ru>
2025-03-13 07:16:37 +0500

    feat(i18n): Add language switching and dynamic UI updates
    - Added language switching button to HomeView.
    - Updated I18N to support dynamic language changes and notify listeners.
    - Modified HomeView to update UI texts when language changes.
    - Improved I18N class to handle translations and listener notifications.
    - The changes improve user experience by allowing dynamic language
      switching and automatic UI updates.

--- src/wse/features/main/view.py
+++ src/wse/features/main/view.py
@@ -2,6 +2,7 @@
 
 import toga
 
+from wse.config.config import Settings
 from wse.core.logger import setup_logger
 from wse.core.navigation.routes import Routes
 from wse.features.shared.observer import Subject
@@ -9,7 +10,7 @@ from wse.features.shared.ui.box import ColumnFlexBox
 from wse.features.shared.ui.button import ButtonStyled
 from wse.features.shared.ui.heading import Heading
 from wse.features.shared.ui.text import MultilineInfoPanel
-from wse.utils.i18n import _
+from wse.utils.i18n import I18N, _
 
 logger = setup_logger('HomeView')
 
@@ -23,6 +24,8 @@ class HomeView(ColumnFlexBox, Subject):
         Subject.__init__(self)
         self._create_widgets()
         self._add_widgets_to_view()
+        self.i18n = I18N(Settings())
+        I18N.add_listener(self)
 
     def _create_widgets(self) -> None:
         self.heading = Heading(_('Home'))
@@ -30,12 +33,16 @@ class HomeView(ColumnFlexBox, Subject):
         self.exercise_button = ButtonStyled(
             _('Exercises'), on_press=self._on_exercises_click
         )
+        self.language_button = ButtonStyled(
+            _('Switch Language'), on_press=self._on_switch_language
+        )
 
     def _add_widgets_to_view(self) -> None:
         self.add(
             self.heading,
             self.info_panel,
             self.exercise_button,
+            self.language_button,
         )
 
     ####################################################################
@@ -43,3 +50,17 @@ class HomeView(ColumnFlexBox, Subject):
 
     def _on_exercises_click(self, _: toga.Widget) -> None:
         self.notify('navigate', route=Routes.EXERCISES)
+
+    def _on_switch_language(self, _: toga.Widget) -> None:
+        current_lang = self.i18n.get_current_language()
+        new_lang = 'ru' if current_lang == 'en' else 'en'
+        self.i18n.set_language(new_lang)
+
+    ####################################################################
+    # Listener methods
+
+    def language_changed(self) -> None:
+        """Update texts when language changes."""
+        self.heading.text = _('Home')
+        self.exercise_button.text = _('Exercises')
+        self.language_button.text = _('Switch Language')
--- src/wse/utils/i18n.py
+++ src/wse/utils/i18n.py
@@ -1,9 +1,10 @@
 """Manages internationalization and language settings."""
 
 import gettext
-from typing import Any, Dict, Optional
+from pathlib import Path
+from typing import Dict, Optional
 
-from typing_extensions import Self
+import toga
 
 from wse.config.config import PROJECT_PATH, Languages, Settings
 from wse.core.logger import setup_logger
@@ -14,59 +15,77 @@ logger = setup_logger('language')
 class I18N:
     """Handles translations and language settings."""
 
-    _instance: Optional['I18N'] = None
-    translations: Dict[str, Any]
-    current_lang: str
-    locale_dir = PROJECT_PATH / 'src' / 'wse' / 'locales'
+    current_lang: str = None
+    translations: Dict[str, gettext.NullTranslations] = {}
+    locale_dir: Path = PROJECT_PATH / 'src' / 'wse' / 'locales'
+    _listeners: list[toga.Box] = []
 
-    def __new__(cls, settings: Optional[Settings] = None) -> Self:
+    def __init__(self, settings: Optional[Settings] = None) -> None:
         """Construct the class."""
-        if not cls._instance:
-            cls._instance = super().__new__(cls)
-            cls._init_languages()
-            cls._instance._init_translations(settings)
-        return cls._instance
+        super().__init__()
+        self.settings = settings
+        self._init_languages()
+        self.set_language(settings.LANGUAGE)
 
     @classmethod
     def _init_languages(cls) -> None:
         """Initialize translations for all supported languages."""
-        cls.translations = {}
-        for language in Languages:
-            cls.translations[language] = gettext.translation(
-                domain='app',
-                localedir=cls.locale_dir,
-                languages=[language],
-                fallback=True,
-            )
+        if not cls.translations:  # Загружаем только один раз
+            for language in Languages:
+                try:
+                    cls.translations[language] = gettext.translation(
+                        domain='app',
+                        localedir=cls.locale_dir,
+                        languages=[language],
+                        fallback=True,
+                    )
+                except FileNotFoundError:
+                    logger.warning(
+                        f'Translation files for {language} not found.'
+                    )
+                    cls.translations[language] = gettext.NullTranslations()
 
-    def _init_translations(self, settings: Optional[Settings]) -> None:
-        """Initialize translations."""
-        self.current_lang = settings.LANGUAGE
-        self.translations[self.current_lang].install()
-
-    def set_language(self, lang: str) -> None:
+    @classmethod
+    def set_language(cls, lang: str) -> None:
         """Set the application language."""
-        if lang in self.translations:
-            self.current_lang = lang
-            self.translations[lang].install()
-            logger.info(f'The application language is set to: {lang}')
+        if lang in cls.translations:
+            cls.current_lang = lang
+            cls.translations[lang].install()
+            logger.info(f'Language changed to: {lang}')
+            cls.notify('language_changed')
         else:
             logger.warning(f'Unsupported language: {lang}')
 
     @classmethod
     def get_current_language(cls) -> str:
         """Get current language."""
-        return cls._instance.current_lang if cls._instance else None
+        return cls.current_lang
 
     def gettext(self, text: str) -> str:
         """Return the translated text."""
-        if self.current_lang in self.translations:
-            return self.translations[self.current_lang].gettext(text)
-        else:
-            logger.warning(
-                f'Language {self.current_lang} not found in Translations'
-            )
-            return text
+        current_lang = self.__class__.current_lang
+        if current_lang and current_lang in self.__class__.translations:
+            return self.__class__.translations[current_lang].gettext(text)
+        logger.warning(f'Language {current_lang} not configured.')
+        return text
+
+    @classmethod
+    def notify(cls, notification: str, **kwargs: object) -> None:
+        """Notify all listeners an event has occurred."""
+        for listener in cls._listeners:
+            try:
+                method = getattr(listener, notification)
+            except AttributeError:
+                method = None
+
+            if method:
+                method(**kwargs)
+
+    @classmethod
+    def add_listener(cls, listener: toga.Box) -> None:
+        """Add a new listener to this data source."""
+        if listener not in cls._listeners:
+            cls._listeners.append(listener)
 
 
 # Global copy of translations
