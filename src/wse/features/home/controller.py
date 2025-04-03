1ab48f27906d4ae2f4523ab75ef733cb020554b8
Sergei Mikurov <454004@mail.ru>
2025-04-04 04:10:36 +0500

    chore(logging): Update message format

--- src/wse/features/home/controller.py
+++ src/wse/features/home/controller.py
@@ -7,7 +7,7 @@ from wse.features.shared.button_names import ButtonName
 
 logging.basicConfig(
     level=logging.DEBUG,
-    format='[%(asctime)s] [%(name)s] [%(levelname)s] [%(message)s]',
+    format='[%(asctime)s] [%(name)s] [%(levelname)s]: %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S',
 )
 logger = logging.getLogger(__name__)
