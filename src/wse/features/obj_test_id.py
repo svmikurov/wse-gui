73814354c4f8faae67753ad87d93abaed650b23c
Sergei Mikurov <454004@mail.ru>
2025-04-04 03:51:09 +0500

    feat(tests): Add test id enumereations
    - Added obj_test_id module with test id enumerations class
    - Allows test a window content assignment with test id

---
+++ src/wse/features/obj_test_id.py
@@ -0,0 +1,10 @@
+"""Application object test ID enumeration."""
+
+from enum import Enum, unique
+
+
+@unique
+class ObjectTestID(str, Enum):
+    """Application object test ID enumeration."""
+
+    HOME_VIEW = 'Home view'
