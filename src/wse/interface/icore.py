39f8b09ffeeb2422e1ea46d29a229c3151decd2d
Sergei Mikurov <454004@mail.ru>
2025-04-04 22:54:41 +0500

    feat(interface): Add core pakege interface module

---
+++ src/wse/interface/icore.py
@@ -0,0 +1,16 @@
+"""Defines protocol interfaces for application components."""
+
+from __future__ import annotations
+
+from typing import Protocol, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from wse.features.shared.button_text import ButtonText
+
+
+class INavigator(Protocol):
+    """Protocol defining the interface for navigator."""
+
+    def navigate(self, button_text: ButtonText) -> None:
+        """Navigate to page by button text."""
+
