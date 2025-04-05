18fa30b89d57f6390940629ea5121a6b01e6a071
Sergei Mikurov <454004@mail.ru>
2025-04-05 16:52:01 +0500

    refactor(tests): Add test ID

--- tests/test_startapp.py
+++ tests/test_startapp.py
@@ -5,6 +5,7 @@ from unittest.mock import MagicMock, patch
 import pytest
 
 from wse.application.app import WSE
+from wse.features.obj_test_id import ObjectTestID
 
 
 @pytest.mark.asyncio
@@ -12,4 +13,6 @@ from wse.application.app import WSE
 async def test_start_app(_: MagicMock) -> None:
     """Test the start application."""
     app = WSE(formal_name='Test App', app_id='org.example.test')
-    assert app.main_window.content == app.page_home.content
+
+    # The application starts with the home page displayed.
+    assert app.main_window.content.test_id is ObjectTestID.HOME_VIEW
