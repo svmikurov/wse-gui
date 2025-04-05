4985b23685651032460b3f91e548290c3d62dec1
Sergei Mikurov <454004@mail.ru>
2025-04-05 16:51:21 +0500

    feat(widgets): Add AppButton

--- src/wse/pages/widgets/__init__.py
+++ src/wse/pages/widgets/__init__.py
@@ -1,10 +1,11 @@
 """General custom application widgets."""
 
 from wse.pages.widgets.box import BoxFlexCol, BoxFlexRow
-from wse.pages.widgets.button import BtnApp, BtnBack
+from wse.pages.widgets.button import AppButton, BtnApp, BtnBack
 from wse.pages.widgets.text import MultilineInfoPanel
 
 __all__ = [
+    'AppButton',
     'BoxFlexCol',
     'BoxFlexRow',
     'BtnApp',
--- src/wse/pages/widgets/button.py
+++ src/wse/pages/widgets/button.py
@@ -67,3 +67,14 @@ class BtnBack(BtnApp):
         super().__init__()
         self.text = 'Назад'
         self.on_press = goto_back_handler
+
+
+class AppButton(toga.Button):
+    """General button."""
+
+    def __init__(self, *args: object, **kwargs: object) -> None:
+        """Construct the button."""
+        super().__init__(*args, **kwargs)
+        self.style.flex = 1
+        self.style.height = BUTTON_HEIGHT
+        self.style.font_size = FONT_SIZE_APP
