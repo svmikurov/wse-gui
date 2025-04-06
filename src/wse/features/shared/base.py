6dae4736f9e71d5a7b7b4e76cd9237197bbdce63
Sergei Mikurov <454004@mail.ru>
2025-04-06 16:22:22 +0500

    GNU nano 6.2                                                                             /home/sv/projects/wse-gui/.git/COMMIT_EDITMSG * fix(features): Remove logger
    - Removed logger from wse/features/shared/base.py

--- src/wse/features/shared/base.py
+++ src/wse/features/shared/base.py
@@ -1,35 +1,10 @@
 """Defines base classes of functions features."""
 
 from dataclasses import dataclass
-from enum import Enum
 
-import toga
-from toga.constants import COLUMN
-
-from wse.features.settings import PADDING_SM
+from wse.core.navigaion.navigator import navigator
+from wse.features.shared.button_text import ButtonText
 from wse.interface.ifeatures import IContent, IController, IModel, IView
-
-
-class BaseBox(toga.Box):
-    """Base pages box.
-
-    Defines a common style for derived box widgets.
-    """
-
-    def __init__(self, *args: object, **kwargs: object) -> None:
-        """Construct the box."""
-        super().__init__(*args, **kwargs)
-        self.style.direction = COLUMN
-        self.style.padding = PADDING_SM
-        self.style.flex = 1
-
-        # Test ID
-        self.test_id: str | None = None
-
-    def __repr__(self) -> str:
-        """Represent a view."""
-        text = super().__repr__()
-        return f'{text[:-1]}:{self.test_id}>'
 
 
 @dataclass
@@ -50,16 +25,7 @@ class BaseController(IController):
         """Return page content."""
         return self.view.content
 
-
-class BaseButtonName(str, Enum):
-    """Base class for creating button enumerations."""
-
-    def __str__(self) -> str:
-        """Return button text."""
-        return self.value
-
-
-class BaseContent(toga.Box):
-    """Base class for creating content."""
-
-    test_id: str | None
+    @staticmethod
+    def navigate(button_text: ButtonText) -> None:
+        """Navigate to page, the button event listener."""
+        navigator.navigate(button_text)
