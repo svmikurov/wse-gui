4a274c14878819130b0ce97fa9e11b73551768a2
Sergei Mikurov <454004@mail.ru>
2025-04-06 16:17:22 +0500

    feat(features): Add object id and rename module
    - Added FOREIGN_TASKS, FOREIGN_TEST object ID
    - Renamed obj_test_id.py module to object_id.py

--- src/wse/features/object_id.py
+++ src/wse/features/object_id.py
@@ -4,8 +4,10 @@ from enum import Enum, unique
 
 
 @unique
-class ObjectTestID(str, Enum):
+class ObjectID(str, Enum):
     """Application object test ID enumeration."""
 
     HOME_VIEW = 'Home view'
     FOREIGN_VIEW = 'Foreign home view'
+    FOREIGN_TASKS = 'Foreign tasks view'
+    FOREIGN_TEST = 'Foreign test view'
