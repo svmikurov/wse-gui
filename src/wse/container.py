f3f76b7cf7cd390ab34c742bd2cae8fad50b65f8
Sergei Mikurov <454004@mail.ru>
2025-04-06 22:01:19 +0500

    feat(deps): Add Foreign tasks MVC dependencies

--- src/wse/container.py
+++ src/wse/container.py
@@ -5,9 +5,11 @@ from dependency_injector import containers, providers
 from wse.core.navigaion.navigator import Navigator
 from wse.features.foreign.home_ctrl import ForeignCtrl
 from wse.features.foreign.home_view import ForeignView
+from wse.features.foreign.tasks_ctrl import TasksController
+from wse.features.foreign.tasks_view import TasksView
 from wse.features.home.controller import HomeController
 from wse.features.home.view import HomeView
-from wse.features.shared.base import BaseBox
+from wse.features.shared.base_ui import BaseContent
 from wse.features.shared.button_text import ButtonText
 
 
@@ -16,7 +18,7 @@ class MainContainer(containers.DeclarativeContainer):
 
     content_box = providers.Dependency()
 
-    home_view = providers.Factory(HomeView, content=content_box)
+    home_view = providers.Factory(HomeView, content_box=content_box)
     home_ctrl = providers.Factory(HomeController, view=home_view)
 
 
@@ -25,15 +27,20 @@ class ForeignContainer(containers.DeclarativeContainer):
 
     content_box = providers.Dependency()
 
-    foreign_view = providers.Factory(ForeignView, content=content_box)
+    foreign_view = providers.Factory(ForeignView, content_box=content_box)
     foreign_ctrl = providers.Factory(ForeignCtrl, view=foreign_view)
+
+    foreign_tasks_view = providers.Factory(TasksView, content_box=content_box)
+    foreign_tasks_ctrl = providers.Factory(
+        TasksController, view=foreign_tasks_view
+    )
 
 
 class FeatureContainer(ForeignContainer):
     """Features package container."""
 
     # Styled general box for content
-    content_box = providers.Factory(BaseBox)
+    content_box = providers.Factory(BaseContent)
 
     main = providers.Container(
         MainContainer,
