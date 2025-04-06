2a70a4d06dc098d0b769fb0221d7a28b5d3d09de
Sergei Mikurov <454004@mail.ru>
2025-04-06 22:17:42 +0500

    refactor(features): Inherit the base controller class

--- src/wse/features/home/controller.py
+++ src/wse/features/home/controller.py
@@ -1,20 +1,10 @@
 """Defines home page controller."""
 
-import logging
 from dataclasses import dataclass
 
-from wse.core.navigaion.navigator import navigator
 from wse.features.shared.base import BaseController
-from wse.features.shared.button_text import ButtonText
-
-logger = logging.getLogger(__name__)
 
 
 @dataclass
 class HomeController(BaseController):
     """Home page controller."""
-
-    def navigate(self, button_text: ButtonText) -> None:
-        """Navigate to page, the button event listener."""
-        logger.debug(f'Pressed {button_text} button')
-        navigator.navigate(button_text)
