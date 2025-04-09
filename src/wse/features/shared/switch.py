058455794f93442a66cf9ebaed382a2e3acf0d2b
Sergei Mikurov <454004@mail.ru>
2025-04-09 15:17:33 +0500

    feat(features): Add custom switch widget
    - Added `switch.py` module
    - Added `SwitchApp` widget at `switch.py` module

---
+++ src/wse/features/shared/switch.py
@@ -0,0 +1,16 @@
+"""Defines general application switch."""
+
+import toga
+from toga.sources import Listener
+
+
+class SwitchApp(toga.Switch, Listener):
+    """General switch."""
+
+    def __init__(self, *args: object, **kwargs: object) -> None:
+        """Construct custom switch."""
+        super().__init__(*args, **kwargs)
+
+    def change(self, item: object) -> None:
+        """Set the initial value from data source for the widget."""
+        self.value = item
