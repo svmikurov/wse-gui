8cfe3dcfddbda45a7bed7a5a1d0e420d2beaf996
Sergei Mikurov <454004@mail.ru>
2025-04-09 15:23:12 +0500

    feat(features): Add progress box

--- src/wse/features/foreign/params_view.py
+++ src/wse/features/foreign/params_view.py
@@ -4,8 +4,15 @@ from wse.core.i18n import _
 from wse.core.navigation.navigation_id import NavigationID
 from wse.features.object_id import ObjectID
 from wse.features.shared.mvc import BaseView
-from wse.features.shared.ui_containers import BaseContent, ColumnBox
-from wse.features.shared.ui_params import SelectionBox, SwitchNumberInputBox
+from wse.features.shared.ui_containers import (
+    BaseContent,
+    ColumnBox,
+)
+from wse.features.shared.ui_params import (
+    ProgressBox,
+    SelectionBox,
+    SwitchNumberInputBox,
+)
 from wse.features.shared.ui_text import MultilineInfoPanel, TitleLabel
 
 
@@ -58,18 +65,25 @@ class ParamsView(BaseView):
     def _add_params_ui(self) -> None:
         self._params_box = ColumnBox()
 
-        # Fill params box
+        # -= Selections =-
         self._params_box.add(
-            # -= Selections =-
             self._category_box,
             self._source_box,
             self._order_box,
             self._start_date_box,
             self._end_date_box,
-            # -= NumberInputs =-
+        )
+
+        # -= NumberInputs =-
+        self._params_box.add(
             self._input_first_box,
             self._input_last_box,
             self._timeout_box,
+        )
+
+        # -= Progress switches =-
+        self._params_box.add(
+            self._progress_box,
         )
 
     def _create_params_ui(self) -> None:
@@ -81,21 +95,28 @@ class ParamsView(BaseView):
         self._end_date_box = SelectionBox()
 
         # -= NumberInputs =-
-        # Count of first items
-        self._input_first_box = SwitchNumberInputBox()
-        # Count of last items
-        self._input_last_box = SwitchNumberInputBox()
+        self._input_first_box = SwitchNumberInputBox()  # Count of first items
+        self._input_last_box = SwitchNumberInputBox()  # Count of last items
         self._timeout_box = SwitchNumberInputBox()
+
+        # -= Progress switches =-
+        self._progress_box = ProgressBox()
 
     def _assign_params_ui_text(self) -> None:
         # -= Selections =-
-        self._category_box.label.text = _('Category')
-        self._source_box.label.text = _('Source')
-        self._order_box.label.text = _('Translate order')
-        self._start_date_box.label.text = _('Period start date')
-        self._end_date_box.label.text = _('Period end date')
+        self._category_box.text = _('Category')
+        self._source_box.text = _('Source')
+        self._order_box.text = _('Translate order')
+        self._start_date_box.text = _('Period start date')
+        self._end_date_box.text = _('Period end date')
 
-        # -= NumberInputs =-
-        self._input_first_box.label.text = _('First number count')
-        self._input_last_box.label.text = _('Last number count')
-        self._timeout_box.label.text = _('Timeout')
+        # -= Number inputs =-
+        self._input_first_box.text = _('First number count')
+        self._input_last_box.text = _('Last number count')
+        self._timeout_box.text = _('Timeout')
+
+        # -= Progress switches =-
+        self._progress_box.study.text = _('Study')
+        self._progress_box.examination.text = _('Examination')
+        self._progress_box.repeat.text = _('Repeat')
+        self._progress_box.know.text = _('Know')
