21a08ffcf226660a05ed718ca4998503e4b339a5
Sergei Mikurov <454004@mail.ru>
2025-04-11 18:37:12 +0500

    feat(deps): Inject dependencies into `core/di_container`
    - Added to `core/di_container` navigator, auth_api, auth_service

--- src/wse/core/di_container.py
+++ src/wse/core/di_container.py
@@ -2,10 +2,36 @@
 
 from dependency_injector import containers, providers
 
+from wse.config.settings import Settings
+from wse.core.api.auth import AuthAPI
+from wse.core.auth.service import AuthService
 from wse.core.navigation.navigator import Navigator
 
 
 class CoreContainer(containers.DeclarativeContainer):
     """Core package container for dependency injection."""
 
-    navigator = providers.Singleton(Navigator)
+    settings = providers.Singleton(Settings)
+
+    # Services
+    navigator = providers.Singleton(
+        Navigator,
+    )
+    auth_api = providers.Factory(
+        AuthAPI,
+    )
+    auth_service = providers.Singleton(
+        AuthService,
+        auth_api=auth_api,
+    )
+
+    # Configuration data
+    endpoints = providers.Configuration(
+        yaml_files=[
+            settings().PROJECT_PATH
+            / 'src'
+            / 'wse'
+            / 'config'
+            / 'endpoints.yml'
+        ]
+    )
