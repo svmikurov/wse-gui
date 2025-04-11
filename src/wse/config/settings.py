3a7d0e0aa0220e0529258223b98bde7e0916d522
Sergei Mikurov <454004@mail.ru>
2025-04-12 00:43:56 +0500

    feat(settings): Add storage config

--- src/wse/config/settings.py
+++ src/wse/config/settings.py
@@ -2,13 +2,27 @@
 
 from pathlib import Path
 
-from pydantic import Field
-from pydantic_settings import BaseSettings
+from pydantic import Field, SecretStr
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 # Paths
 MODULE_PATH = Path(__file__).resolve().parent
 PROJECT_PATH = MODULE_PATH.parents[2]
 ENV_PATH = PROJECT_PATH / '.env'
+
+
+class StorageConfig(BaseSettings):
+    """Stores configuration for storage-related settings."""
+
+    token_file: Path = PROJECT_PATH / 'data' / 'secrets' / 'token.enc'
+    encryption_key: SecretStr
+
+    model_config = SettingsConfigDict(
+        env_prefix='STORAGE_',
+        env_file=ENV_PATH,
+        env_file_encoding='utf-8',
+        extra='ignore',
+    )
 
 
 class Settings(BaseSettings):
@@ -19,3 +33,6 @@ class Settings(BaseSettings):
 
     # URL
     base_url: str = Field(default='http://wselfedu.online')
+
+    # Configs
+    storage_config: StorageConfig = Field(default_factory=StorageConfig)
