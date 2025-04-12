4c7f0dea081630d15da9cc1d4616635baf695932
Sergei Mikurov <454004@mail.ru>
2025-04-12 17:46:20 +0500

    fix(features): Fix HomeModel attribute annotation
    - Fixed `context` property annotation in HomeModel to HomeContext.

--- src/wse/features/main/home_model.py
+++ src/wse/features/main/home_model.py
@@ -20,6 +20,8 @@ class User:
 class HomeModel(BaseModel):
     """Home page model."""
 
+    _context: HomeContext
+
     def __init__(self, subject: ISubject) -> None:
         """Construct the model."""
         super().__init__(subject)
@@ -33,3 +35,8 @@ class HomeModel(BaseModel):
         logger.debug('Called `_render_context` method of `HomeModel`')
 
         self.subject.notify('fill_info_panel', value=self.context.info_panel)
+
+    @property
+    def context(self) -> HomeContext:
+        """View context."""
+        return self._context
