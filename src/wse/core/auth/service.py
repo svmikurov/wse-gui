29f8401a855628b47b5043fd0e0b11ad5581d082
Sergei Mikurov <454004@mail.ru>
2025-04-13 16:55:35 +0500

    feat(auth): Add a stub method authentication

--- src/wse/core/auth/service.py
+++ src/wse/core/auth/service.py
@@ -20,6 +20,10 @@ class AuthService:
         self._token_storage = token_storage
         self._token: str | None = None
 
+    def authenticate(self, credentials: dict[str, str]) -> None:
+        """Authenticate the user."""
+        logger.debug('Called `authenticate` method')
+
     def check_auth(self) -> bool:
         """Check if the user is authenticated."""
         if not self._token:
