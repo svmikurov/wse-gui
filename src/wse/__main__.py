88e304b5b6e64c06e0bc5fe691034abe8b09e5ed
Sergei Mikurov <454004@mail.ru>
2025-04-23 01:09:07 +0500

    refactor(logging): Move baseConfig into `__main__`

--- src/wse/__main__.py
+++ src/wse/__main__.py
@@ -1,6 +1,16 @@
 """Start app."""
 
+import logging
+
 from wse.app import main
+
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='[%(asctime)s] [%(name)s] [%(levelname)s]: %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+)
+
 
 if __name__ == '__main__':
     main().main_loop()
--- src/wse/app.py
+++ src/wse/app.py
@@ -1,29 +1,15 @@
 """WSE application."""
 
-import logging
-
 import toga
 
-from wse.constants import SCREEN_SIZE
 from wse.core.navigation.navigation_id import NavigationID
 from wse.di_container import AppContainer
 from wse.interface.icore import IAuthService, INavigator
-from wse.menu import MenuMixin
-from wse.models.user import User
-from wse.sources.text_panel_main import SourceMainPanel
-
-logging.basicConfig(
-    level=logging.DEBUG,
-    format='[%(asctime)s] [%(name)s] [%(levelname)s]: %(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S',
-)
 
 
-class WSE(MenuMixin, toga.App):
+class WSE(toga.App):
     """WSE application."""
 
-    user: User
-    source_main_panel: SourceMainPanel
     _auth_service: IAuthService
     _container: AppContainer
     _navigator: INavigator
@@ -31,13 +17,11 @@ class WSE(MenuMixin, toga.App):
     def startup(self) -> None:
         """Construct and show the application."""
         # Models
-        self.user = User()
-        self.source_main_panel = SourceMainPanel(self.user)
 
         # Application start with Main pages box content.
         self.main_window = toga.MainWindow(
             title=self.formal_name,
-            size=toga.Size(*SCREEN_SIZE),
+            size=toga.Size(440, 700),
         )
         self.main_window.show()
 
