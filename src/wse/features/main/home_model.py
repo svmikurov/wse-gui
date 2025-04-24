7b74aa70d8d34c92f0fad5e17f354afae308052b
Sergei Mikurov <454004@mail.ru>
2025-04-24 05:26:58 +0500

    refactor(main): Update `HomeModel` class structure

--- src/wse/features/main/home_model.py
+++ src/wse/features/main/home_model.py
@@ -2,6 +2,7 @@
 
 import logging
 
+from wse.core.api.client import ApiClient
 from wse.features.base.context import HomeContext
 from wse.features.base.mvc import BaseModel
 from wse.interface.ifeatures import ISubject
@@ -12,23 +13,26 @@ logger = logging.getLogger(__name__)
 class HomeModel(BaseModel):
     """Home page model."""
 
+    _api_client: ApiClient
     _context: HomeContext
+    _subject: ISubject
 
-    def __init__(self, subject: ISubject) -> None:
+    def __init__(self, subject: ISubject, api_client: ApiClient) -> None:
         """Construct the model."""
-        super().__init__(subject)
+        super().__init__(subject=subject, api_client=api_client)
         self._context = HomeContext()
 
     def _set_context(self) -> None:
         pass
 
-    def _notify_context_render(self) -> None:
-        self.subject.notify('fill_info_panel', value=self.context.info_panel)
-
     @property
     def context(self) -> HomeContext:
         """View context."""
         return self._context
+
+    # Notifications
+    def _notify_context_render(self) -> None:
+        self.subject.notify('fill_info_panel', value=self.context.info_panel)
 
     def _notify_render_context(self) -> None:
         """Temporary unused."""
