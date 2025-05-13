8b9578e898ec4f666837bcc43ff118d7b04f2a7d
Sergei Mikurov <454004@mail.ru>
2025-05-13 16:25:49 +0500

    fix(app): Fix logging setup

--- src/wse/__main__.py
+++ src/wse/__main__.py
@@ -1,10 +1,11 @@
 """Start app."""
 
-from wse.app import main
 from wse.core.logger.setup import setup_logging
 
 setup_logging()
 
+# ruff: noqa: E402
+from wse.app import main
 
 if __name__ == '__main__':
     main().main_loop()
