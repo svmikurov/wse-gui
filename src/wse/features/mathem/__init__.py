b2ff1ebc87e022c0978763a020fc86c812999c59
Sergei Mikurov <454004@mail.ru>
2025-05-13 16:29:48 +0500

    fix(mathem) Remove legacy code

--- src/wse/features/mathem/__init__.py
+++ src/wse/features/mathem/__init__.py
@@ -1,13 +1,13 @@
 """Mathematical page package."""
 
-from wse.features.mathem.calculation_controller import (
+from wse.features.mathem.pages.calculation_controller import (
     CalculationController,
 )
-from wse.features.mathem.calculation_model import CalculationModel
-from wse.features.mathem.calculation_view import CalculationView
-from wse.features.mathem.mathem_controller import MathematicalController
-from wse.features.mathem.mathem_model import MathematicalModel
-from wse.features.mathem.mathem_view import MathematicalView
+from wse.features.mathem.pages.calculation_model import CalculationModel
+from wse.features.mathem.pages.calculation_view import CalculationView
+from wse.features.mathem.pages.mathem_controller import MathematicalController
+from wse.features.mathem.pages.mathem_model import MathematicalModel
+from wse.features.mathem.pages.mathem_view import MathematicalView
 
 __all__ = [
     'CalculationModel',
