beba08016d31f43b0ba12088805f2456b19d4f8c
Sergei Mikurov <454004@mail.ru>
2025-06-23 02:45:05 +0500

    docs: Update requirements

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,18 @@ release = '0.4.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions: list[str] = []
+extensions: list[str] = [
+    # Include documentation from docstrings
+    # https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html#module-sphinx.ext.autodoc
+    'sphinx.ext.autodoc',
+    # Add copy button from block
+    # https://sphinx-copybutton.readthedocs.io/en/latest/#sphinx-copybutton
+    'sphinx_copybutton',
+    # Allow reference sections using its title
+    # https://www.sphinx-doc.org/en/master/usage/extensions/autosectionlabel.html#module-sphinx.ext.autosectionlabel
+    'sphinx.ext.autosectionlabel',
+    'sphinx.ext.todo',
+]
 
 templates_path = ['_templates']
 exclude_patterns: list[str] = []
@@ -24,5 +35,5 @@ exclude_patterns: list[str] = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'furo'
 html_static_path = ['_static']
