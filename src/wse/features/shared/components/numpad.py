7c0c638ccdcafa3530f92a5144cfa88e2ae9c74d
Sergei Mikurov <454004@mail.ru>
2025-06-26 14:53:22 +0500

    feat(features): Add outer box for NumPad

--- src/wse/features/shared/components/numpad.py
+++ src/wse/features/shared/components/numpad.py
@@ -16,15 +16,20 @@ from ...shared.components.interfaces import INumPadContainer, INumPadModel
 
 logger = logging.getLogger(__name__)
 
+MAX_SYMBOL_COUNT = 8
+NO_TEXT = ''
+
+# Numpad signs
+BACKSPACE = '\u232b'
+DOTE = '\u002e'
+MINUS = '\u2014'
+
 
 @inject
 class NumPadModel(
     AddObserverMixin,
 ):
     """Model for NumPad container."""
-
-    NO_TEXT = ''
-    MAX_NUMBER_LENGTH = 8
 
     def __init__(
         self,
@@ -34,22 +39,22 @@ class NumPadModel(
         self._subject = subject
         self._input: str = ''
 
-    def update_input(self, value: str) -> None:
+    def update_input(self, symbol: str) -> None:
         """Update the user input."""
-        if value == '⌫' and self._input == '0.':
-            self._input = self.NO_TEXT
-        elif value == '⌫':
+        if symbol == BACKSPACE and self._input == '0.':
+            self._input = NO_TEXT
+        elif symbol == BACKSPACE:
             self._input = self._input[:-1]
-        elif len(self._input) >= self.MAX_NUMBER_LENGTH:
+        elif len(self._input) >= MAX_SYMBOL_COUNT:
             return
-        elif value == '.' and value in self._input:
+        elif symbol == DOTE and symbol in self._input:
             return
-        elif value == '.' and self._input == self.NO_TEXT:
+        elif symbol == DOTE and self._input == NO_TEXT:
             self._input += '0.'
         elif self._input == '0':
-            self._input += '.' + value
+            self._input += DOTE + symbol
         else:
-            self._input += value
+            self._input += symbol
 
         self._subject.notify('numpad_input_updated', value=self._input)
 
@@ -67,20 +72,16 @@ class NumPadContainer(
     _theme_config: NumPadThemeConfig
 
     def _setup(self) -> None:
-        self._build_num_box()
-        self._build_sign_box()
+        self._subject.add_observer(self._model)
+        self._build_boxes()
         self.update_style(self._style_config)
         self.update_style(self._theme_config)
-        self._subject.add_observer(self._model)
 
     def _populate_content(self) -> None:
-        self.content.style.update(direction='row', flex=1)
-        self.content.add(
-            self._num_box,
-            self._sign_box,
-        )
+        self.content.add(self._outer_box)
 
     def _create_ui(self) -> None:
+        # Create buttons
         self._btn_1 = self._create_button(1)
         self._btn_2 = self._create_button(2)
         self._btn_3 = self._create_button(3)
@@ -91,9 +92,14 @@ class NumPadContainer(
         self._btn_8 = self._create_button(8)
         self._btn_9 = self._create_button(9)
         self._btn_0 = self._create_button(0)
-        self._btn_backspace = self._create_button('\u232b')
-        self._btn_dote = self._create_button('\u002e')
-        self._btn_minus = self._create_button('\u2014')
+        self._btn_backspace = self._create_button(BACKSPACE)
+        self._btn_dote = self._create_button(DOTE)
+        self._btn_minus = self._create_button(MINUS)
+
+    def _build_boxes(self) -> None:
+        self._build_num_box()
+        self._build_sign_box()
+        self._build_outer_box()
 
     # TODO: Remove `localize_ui()` method dependency (SOLID error)
     def localize_ui(self) -> None:
@@ -104,6 +110,7 @@ class NumPadContainer(
         self, config: NumPadStyleConfig | NumPadThemeConfig
     ) -> None:
         """Update widgets style."""
+        # Update buttons style
         for row in self._num_box.children:
             for button in row.children:
                 button.style.update(**config.button)
@@ -111,16 +118,21 @@ class NumPadContainer(
         for button in self._sign_box.children:
             button.style.update(**config.button)
 
+        # Update outer NumPad box style
+        self._outer_box.style.update(**config.outer_box)
+
     # Utility methods
 
     def _create_button(self, text: str | int) -> toga.Button:
-        return toga.Button(
-            text=str(text),
-            on_press=self._handle_button_press,
-        )
+        return toga.Button(text=str(text), on_press=self._handle_button_press)
 
     def _handle_button_press(self, button: toga.Button) -> None:
         self._subject.notify('button_pressed', value=button.text)
+
+    def _build_outer_box(self) -> None:
+        """Create outer box for NumPad."""
+        # NamPad is wrapped in box
+        self._outer_box = toga.Box(children=[self._num_box, self._sign_box])
 
     def _build_num_box(self) -> None:
         self._num_box = toga.Row(
@@ -134,10 +146,16 @@ class NumPadContainer(
 
     def _build_sign_box(self) -> None:
         self._sign_box = FlexColumn(
-            children=[self._btn_backspace, self._btn_minus, self._btn_dote]
+            children=[
+                self._btn_backspace,
+                self._btn_minus,
+                self._btn_dote,
+            ]
         )
 
 
+# TODO: Add base controller without navigate feature
+#  (to avoid SOLID error)
 @inject
 @dataclass
 class NumPadController(
