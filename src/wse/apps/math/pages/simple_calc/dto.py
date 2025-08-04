e8b0906697bab84d9721d145366f2983b84c5ef0
Sergei Mikurov <454004@mail.ru>
2025-08-04 22:35:24 +0500

    feat(apps/math): Add dto
    - Added dto classes for calculation exercise

---
+++ src/wse/apps/math/pages/simple_calc/dto.py
@@ -0,0 +1,41 @@
+"""Defines data model fo Calculation page."""
+
+import uuid
+
+from pydantic import BaseModel
+from wse_exercises.base.mixins import ConvertMixin
+
+# TODO: Fix combined `ConvertMixin` and `BaseModel` to `BaseSchema'
+
+class CalcConfDTO(ConvertMixin, BaseModel):
+    """Calculation task config DTO."""
+
+    min_value: str
+    max_value: str
+
+
+class CalcDataDTO(ConvertMixin, BaseModel):
+    """Calculation exercise data model."""
+
+    exercise_name: str
+
+
+class CalcTaskDTO(ConvertMixin, BaseModel):
+    """Calculation task DTO."""
+
+    uid: uuid.UUID
+    question: str
+
+
+class CalcAnswerDTO(ConvertMixin, BaseModel):
+    """Calculation task user answer DTO."""
+
+    uid: uuid.UUID
+    answer: str
+
+
+class CalcResultDTO(ConvertMixin, BaseModel):
+    """Calculation task user answer checking result DTO."""
+
+    is_correct: bool
+    correct_answer: str
