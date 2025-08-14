1e70846183c8c5b780c3caecb2df1eb862e93c5e
Sergei Mikurov <454004@mail.ru>
2025-08-14 05:41:56 +0500

    refactor(apps/routes): Update import
    - Updated relative import to absolute

--- src/wse/apps/math/routes.py
+++ src/wse/apps/math/routes.py
@@ -4,8 +4,8 @@ from typing import no_type_check
 
 from wse.apps.nav_id import NavID
 from wse.features.base import BaseRoutes
+from wse.features.interfaces.imvc import IPageController
 
-from ...features.interfaces.imvc import IPageController
 from .pages.index.interfaces import IIndexMathController
 from .pages.simple_calc.interfaces import ICalcController
 
