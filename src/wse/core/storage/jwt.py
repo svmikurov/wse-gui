e1865b9a253bedf2788c8e2c3cdb38bec9c3fc17
Sergei Mikurov <454004@mail.ru>
2025-08-14 23:39:24 +0500

    feat(core/storage): Add attr to constructor
    - Added JSON path paramenter to `JWTJsonStorage`

--- src/wse/core/storage/jwt.py
+++ src/wse/core/storage/jwt.py
@@ -2,6 +2,7 @@
 
 import json
 import logging
+from pathlib import Path
 
 from typing_extensions import override
 
@@ -9,17 +10,22 @@ from wse.config.settings import RESOURCES_PATH
 from wse.core.exceptions import StorageError
 from wse.core.interfaces.istorage import IJWTJsonStorage
 
-JSON_PATH = RESOURCES_PATH / '.storage' / 'jwt.json'
-
 logger = logging.getLogger(__name__)
 
 
-# Use only for development
 class JWTJsonStorage(IJWTJsonStorage):
     """JWT storage with JSON."""
 
-    def __init__(self) -> None:
+    FALLBACK_PATH = RESOURCES_PATH / '.storage' / 'jwt.json'
+
+    def __init__(self, json_path: Path | str | None = None) -> None:
         """Construct the storage."""
+        if json_path is None:
+            self._json_path = self.FALLBACK_PATH
+            logger.warning('JSON path for tokens not passed, fallback  used')
+        else:
+            self._json_path = Path(json_path)
+
         self._access_token: str | None = None
         self._refresh_token: str | None = None
 
@@ -27,9 +33,9 @@ class JWTJsonStorage(IJWTJsonStorage):
     def save_tokens(self, access: str, refresh: str) -> None:
         """Save JWT tokens."""
         try:
-            JSON_PATH.parent.mkdir(parents=True, exist_ok=True)
+            self._json_path.parent.mkdir(parents=True, exist_ok=True)
 
-            with open(JSON_PATH, 'w', encoding='utf-8') as f:
+            with open(self._json_path, 'w', encoding='utf-8') as f:
                 tokens = {'access': access, 'refresh': refresh}
                 json.dump(tokens, f, indent=4, ensure_ascii=False)
 
@@ -49,7 +55,7 @@ class JWTJsonStorage(IJWTJsonStorage):
             return self._access_token
 
         try:
-            with open(JSON_PATH, 'r', encoding='utf-8') as f:
+            with open(self._json_path, 'r', encoding='utf-8') as f:
                 token: str = json.load(f).get('access')
                 self._access_token = token
                 return token
@@ -66,12 +72,12 @@ class JWTJsonStorage(IJWTJsonStorage):
     def access_token(self, token: str) -> None:
         """Get access storage token."""
         try:
-            with open(JSON_PATH, 'r', encoding='utf-8') as f:
+            with open(self._json_path, 'r', encoding='utf-8') as f:
                 tokens = json.load(f)
 
             tokens['access'] = token
 
-            with open(JSON_PATH, 'w', encoding='utf-8') as f:
+            with open(self._json_path, 'w', encoding='utf-8') as f:
                 json.dump(tokens, f, indent=4, ensure_ascii=False)
 
         except Exception as e:
@@ -89,7 +95,7 @@ class JWTJsonStorage(IJWTJsonStorage):
             return self._refresh_token
 
         try:
-            with open(JSON_PATH, 'r', encoding='utf-8') as f:
+            with open(self._json_path, 'r', encoding='utf-8') as f:
                 token: str = json.load(f).get('refresh')
                 self._refresh_token = token
                 return token
@@ -102,7 +108,7 @@ class JWTJsonStorage(IJWTJsonStorage):
     def delete_tokens(self, missing_ok: bool = True) -> None:
         """Delete JWT tokens."""
         try:
-            JSON_PATH.unlink(missing_ok=missing_ok)
+            self._json_path.unlink(missing_ok=missing_ok)
 
         except Exception as e:
             logger.exception('Error deleting tokens: %s', e)
