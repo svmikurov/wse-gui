3130460626da058e5c63d19a984e45768d70926f
Sergei Mikurov <454004@mail.ru>
2025-09-19 17:40:35 +0500

    fix(feature): Remove legacy code

--- src/wse/feature/shared/containers/interfaces.py
+++ src/wse/feature/shared/containers/interfaces.py
@@ -1,13 +1,9 @@
 """Defines protocols for containers interfaces."""
 
-from typing import Any, Iterable
-
 from typing_extensions import Protocol
 
 from wse.config.layout import StyleConfig, ThemeConfig
 from wse.feature.interfaces.icontainer import Containerizable
-from wse.feature.interfaces.icontent import GetContentProto
-from wse.feature.interfaces.iobserver import AddObserverProto
 
 # Text task container
 
@@ -39,28 +35,3 @@ class TextTaskContainerProto(
     @property
     def answer(self) -> str:
         """Get text."""
-
-
-# Select container
-
-
-class SelectionContainerProto(
-    AddObserverProto,
-    Containerizable,
-    Protocol,
-):
-    """Protocol for Select container interface."""
-
-    def update_items(self, items: Iterable[Any]) -> None:
-        """Update select items."""
-
-
-class SelectionControllerProto(
-    AddObserverProto,
-    GetContentProto,
-    Protocol,
-):
-    """Protocol for Select container controller interface."""
-
-    def update_items(self, items: Iterable[Any]) -> None:
-        """Update select items."""
