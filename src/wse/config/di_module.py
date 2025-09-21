e7d781716f7a8b9958ebd1c89f0062d49a509ae6
Sergei Mikurov <454004@mail.ru>
2025-09-21 18:22:45 +0500

    feat(config/di): Add logging
    - Added logging of 'ValidationError' for API config dependency

--- src/wse/config/di_module.py
+++ src/wse/config/di_module.py
@@ -1,8 +1,10 @@
 """Defines Configuration injection module."""
 
 import json
+import logging
 
 from injector import Module, provider, singleton
+from pydantic import ValidationError
 
 from wse.config.api_paths import APIConfigV1, MathAPIConfigV1
 from wse.config.layout import StyleConfig, ThemeConfig
@@ -20,6 +22,8 @@ LAYOUT_THEME_PATH = STYLE_PATH / LAYOUT_THEME
 API_CONFIG_PATH = CONFIGS_PATH / 'api.json'
 MATH_API_CONFIG_PATH = CONFIGS_PATH / 'api_math.json'
 
+log = logging.getLogger(__name__)
+
 
 class ConfigModule(Module):
     """Configuration injection module."""
@@ -29,7 +33,11 @@ class ConfigModule(Module):
     def provide_api_config(self) -> APIConfigV1:
         """Provide API configuration."""
         with open(API_CONFIG_PATH, 'r') as f:
-            return APIConfigV1(**json.load(f))
+            try:
+                return APIConfigV1(**json.load(f))
+            except ValidationError:
+                log.exception('Load API config error')
+                raise
 
     @provider
     @singleton
