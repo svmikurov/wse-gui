2a560e7ab4d4c0ac6d39725d94971214147569c7
Sergei Mikurov <454004@mail.ru>
2025-10-24 07:52:05 +0500

    feat(feature/observer): Add ABC observer via accessor
    - Added `AccessorABC` for listener via accessor
    - Added `AccessorNotifyChangeABC` for notifications via accessors

--- src/wse/feature/observer/abc.py
+++ src/wse/feature/observer/abc.py
@@ -3,6 +3,8 @@
 from abc import ABC, abstractmethod
 from typing import override
 
+import toga
+
 from .generic import NotifyGenABC, ObserverManagerGenABC
 
 
@@ -41,3 +43,27 @@ class SubjectABC(
     ABC,
 ):
     """ABC for Subject of Observer pattern."""
+
+
+# Observer via accessor
+# ---------------------
+
+
+class AccessorABC(ABC):
+    """ABC for listener via accessor."""
+
+    @abstractmethod
+    def _check_accessors(self) -> None:
+        """Check that UI attr name corresponding to accessor."""
+
+    @abstractmethod
+    def _get_ui(self, accessor: str) -> toga.Widget:
+        """Get UI via accessor."""
+
+
+class AccessorNotifyChangeABC(ABC):
+    """ABC for notifications via accessors."""
+
+    @abstractmethod
+    def change(self, accessor: str, value: str) -> None:
+        """Change ui content via accessor."""
