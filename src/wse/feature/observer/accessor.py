9f853f9c5e424f23bcb5645cc800a3563c619bf2
Sergei Mikurov <454004@mail.ru>
2025-10-26 23:22:44 +0500

    feat(feature/observer): Add check on has accessor
    - Added `_has_accessor` method to `AccessorMixin`

--- src/wse/feature/observer/accessor.py
+++ src/wse/feature/observer/accessor.py
@@ -11,19 +11,13 @@ class AccessorMixin:
     def _check_accessors(self) -> None:
         """Check that UI attr name corresponding to accessor."""
         if not self._accessors:
-            raise AttributeError(
-                f'Not implemented `_accessors` attribute in '
-                f'`{self.__class__.__name__}` for observer pattern'
+            raise RuntimeError(
+                f'Class `{self.__class__.__name__}` must define non-empty '
+                f'`_accessors. Got: {self._accessors!r}`'
             )
 
-        for attr in self._accessors:
-            attr_name = f'_{attr}'
-
-            if not hasattr(self, attr_name):
-                raise AttributeError(
-                    f"Not implemented attribute '{f'_{attr_name}'}' in "
-                    f'`{self.__class__.__name__}` for observer pattern'
-                )
+        for accessor in self._accessors:
+            self._has_accessor(f'_{accessor}')
 
     def _get_ui(self, accessor: str) -> toga.Widget:
         """Get UI via accessor."""
@@ -31,7 +25,13 @@ class AccessorMixin:
             return getattr(self, f'_{accessor}')  # type: ignore[no-any-return]
 
         except AttributeError:
-            raise ValueError(
-                f"Got unexpected accessor '{accessor}' for "
-                f'`{self.__class__.__name__}`. Available: {self._accessors}'
+            raise LookupError(
+                f"Unknown accessor '{accessor}'. Available: {self._accessors}"
             ) from None
+
+    def _has_accessor(self, accessor: str) -> None:
+        if not hasattr(self, accessor):
+            raise AttributeError(
+                f"Not implemented attribute '{f'_{accessor}'}' in "
+                f'`{self.__class__.__name__}` for observer pattern'
+            )
