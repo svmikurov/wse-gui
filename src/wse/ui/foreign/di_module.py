514402255b872d5db6db51cca570be8af25206a2
Sergei Mikurov <454004@mail.ru>
2025-10-26 23:29:41 +0500

    feat(ui/foreign): Add word study UI data state
    - Added `WordStudyData` UI data state

--- src/wse/ui/foreign/di_module.py
+++ src/wse/ui/foreign/di_module.py
@@ -2,13 +2,13 @@
 
 from typing import no_type_check
 
-from injector import Binder, Module
+from injector import Binder, Module, SingletonScope
 
 from .index import IndexForeignViewABC, IndexForeignViewModelABC
 from .index.state import IndexForeignViewModel
 from .index.view import IndexForeignView
 from .params import WordStudyParamsViewABC, WordStudyParamsViewModelABC
-from .params.state import WordStudyParamsViewModel
+from .params.state import WordStudyData, WordStudyParamsViewModel
 from .params.view import WordStudyParamsView
 from .study import StudyForeignViewABC, StudyForeignViewModelABC
 from .study.state import StudyForeignViewModel
@@ -30,5 +30,6 @@ class ForeignModule(Module):
         binder.bind(StudyForeignViewABC, to=StudyForeignView)
 
         # Word study params
+        binder.bind(WordStudyData, scope=SingletonScope)
         binder.bind(WordStudyParamsViewModelABC, to=WordStudyParamsViewModel)
         binder.bind(WordStudyParamsViewABC, to=WordStudyParamsView)
--- src/wse/ui/foreign/params/state.py
+++ src/wse/ui/foreign/params/state.py
@@ -1,14 +1,23 @@
 """Word study params state."""
 
-from dataclasses import dataclass
+from dataclasses import dataclass, replace
 from typing import Any
 
 from injector import inject
 
 from wse.feature.observer.mixins import NotifyAccessorGen, ObserverManagerGen
 from wse.ui.base.navigate.mixin import NavigateStateMixin
+from wse.ui.containers.params.entity import NamedEntity
 
 from . import WordStudyParamsViewModelABC
+
+
+@dataclass
+class WordStudyData:
+    """Word study data."""
+
+    mark_select: NamedEntity | None = None
+    category_select: NamedEntity | None = None
 
 
 @inject
@@ -20,3 +29,13 @@ class WordStudyParamsViewModel(
     WordStudyParamsViewModelABC,
 ):
     """Word study params ViewModel."""
+
+    _data: WordStudyData
+
+    def update(self, accessor: str, value: object) -> None:
+        """Update state."""
+        self._update_data({accessor: value})
+
+    # TODO: Fix type ignore
+    def _update_data(self, data: dict[str, object]) -> None:
+        self._data = replace(self._data, **data)  # type: ignore[arg-type]
