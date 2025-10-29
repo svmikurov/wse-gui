b612952865fd3e5aeb06507315a4d57495b3dc27
Sergei Mikurov <454004@mail.ru>
2025-10-29 19:20:34 +0500

    refactor(ui/container): Update control container

--- src/wse/ui/base/container/__init__.py
+++ src/wse/ui/base/container/__init__.py
@@ -3,7 +3,9 @@
 __all__ = [
     'ContainerABC',
     'StyleABC',
+    'LocalizeABC',
 ]
 
 from .abc import ContainerABC
+from .abc_locale import LocalizeABC
 from .abc_style import StyleABC
--- src/wse/ui/containers/control/container.py
+++ src/wse/ui/containers/control/container.py
@@ -1,15 +1,30 @@
 """Exercise control container."""
 
 from dataclasses import dataclass
+from typing import TYPE_CHECKING
 
 import toga
 from injector import inject
 
 from wse.config.layout import StyleConfig, ThemeConfig
+from wse.core.base.enums import BaseEnum
 from wse.ui.base.content.mixins import GetContentMixin
+from wse.utils import decorators
 from wse.utils.i18n import I18N
 
 from . import ControlContainerABC
+
+if TYPE_CHECKING:
+    from toga.widgets.button import OnPressHandler
+
+
+class Action(BaseEnum):
+    """Exercise action enumeration."""
+
+    PAUSE = I18N.EXERCISE('Pause')
+    NEXT = I18N.EXERCISE('Next')
+    KNOWN = I18N.EXERCISE('Known')
+    UNKNOWN = I18N.EXERCISE('Unknown')
 
 
 @inject
@@ -27,10 +42,10 @@ class ControlContainer(
         )
 
     def _create_ui(self) -> None:
-        self._btn_next = toga.Button(I18N.EXERCISE('Next'))
-        self._btn_pause = toga.Button(I18N.EXERCISE('Pause'))
-        self._btn_known = toga.Button(I18N.EXERCISE('Known'))
-        self._btn_unknown = toga.Button(I18N.EXERCISE('Unknown'))
+        self._btn_next = self._create_btn(Action.NEXT)
+        self._btn_pause = self._create_btn(Action.PAUSE)
+        self._btn_known = self._create_btn(Action.KNOWN)
+        self._btn_unknown = self._create_btn(Action.UNKNOWN)
 
     def _update_style(self, config: StyleConfig | ThemeConfig) -> None:
         style = config.control
@@ -39,3 +54,12 @@ class ControlContainer(
         self._btn_pause.style.update(**style.left_btn)
         self._btn_known.style.update(**style.left_btn)
         self._btn_unknown.style.update(**style.right_btn)
+
+    def _create_btn(self, text: Action) -> toga.Button:
+        return toga.Button(text, on_press=self._on_press)  # type: ignore[arg-type]
+
+    # TODO: Implement method
+    @decorators.log_unimplemented_call
+    def _on_press(self, button: toga.Button) -> OnPressHandler:  # type: ignore[empty-body]
+        """Button callback."""
+        pass
