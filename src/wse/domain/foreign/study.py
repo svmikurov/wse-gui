34624823380520641b754a6451ba875e08cd4f48
Sergei Mikurov <454004@mail.ru>
2025-11-07 10:45:12 +0500

    refactor(domain/foreign): Update `WordStudyUseCase`

--- src/wse/domain/foreign/study.py
+++ src/wse/domain/foreign/study.py
@@ -7,13 +7,9 @@ from typing import override
 
 from injector import inject
 
-from wse.data.repositories import foreign as repo
+from wse.data.repositories import foreign as repos
 from wse.data.sources.foreign import schemas
-from wse.domain.foreign.abc import (
-    ExerciseAccessorT,
-    UIStateNotifyT,
-    WordStudyUseCaseABC,
-)
+from wse.domain import foreign as base
 from wse.feature.observer.accessor import NotifyAccessorGen
 
 from ..abc import PresentationABC
@@ -26,29 +22,26 @@ log = logging.getLogger(__name__)
 @inject
 @dataclass
 class WordStudyUseCase(
-    WordStudyUseCaseABC,
-    NotifyAccessorGen[UIStateNotifyT, ExerciseAccessorT],
+    base.WordStudyUseCaseABC,
+    NotifyAccessorGen[base.UIStateNotifyT, base.ExerciseAccessorT],
 ):
     """Words study Use Case."""
 
-    _get_word_repo: repo.WordStudyCaseRepoABC
-    _progress_repo: repo.WordStudyProgressRepoABC
-    _settings_repo: repo.WordStudySettingsRepoABC
+    _get_word_repo: repos.WordStudyCaseRepoABC
+    _progress_repo: repos.WordStudyProgressRepoABC
     _domain: PresentationABC
-
-    def __post_init__(self) -> None:
-        """Construct the case."""
-        self._settings = self._settings_repo.get_settings()
 
     def start(self) -> None:
         """Start exercise."""
-        asyncio.create_task(self._loop_word_study())
+        self._create_task()
         self._domain.start()
+
+    def stop(self) -> None:
+        """Stop exercise."""
+        self._domain.stop()
 
     async def _loop_word_study(self) -> None:
         """Loop Word study exercise."""
-        asyncio.create_task(self._monitor_progress())
-
         while True:
             # Start presentation case
             await self._domain.wait_start_case_event()
@@ -115,12 +108,12 @@ class WordStudyUseCase(
         """Handle 'unknown' case user action of exercise."""
         self._progress_repo.decrement()
 
-    def stop(self) -> None:
-        """Stop exercise."""
-        self._domain.stop()
-
     # Utility methods
     # ---------------
+
+    def _create_task(self) -> None:
+        asyncio.create_task(self._loop_word_study())
+        asyncio.create_task(self._monitor_progress())
 
     def _get_data(self) -> schemas.WordPresentationSchema | None:
         try:
