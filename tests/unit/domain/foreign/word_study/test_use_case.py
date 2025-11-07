5fc45d0f8792b8b26b041b63d2b1cb4d7493dde3
Sergei Mikurov <454004@mail.ru>
2025-11-07 10:37:56 +0500

    test(unit/domain): Fix test of `WordStudyUseCase.start`

--- tests/unit/domain/foreign/word_study/test_use_case.py
+++ tests/unit/domain/foreign/word_study/test_use_case.py
@@ -1,10 +1,22 @@
 """Test Word study UseCase."""
 
+from unittest.mock import AsyncMock, patch
+
+from wse.domain.foreign.study import WordStudyUseCase
+
 
 class TestUseCaseStart:
     """Test start UseCase call."""
 
     def test_start(
         self,
+        mock_domain: AsyncMock,
+        use_case: WordStudyUseCase,
     ) -> None:
-        """Test correct call `start()` method."""
+        """Test call `start()` method."""
+        with patch(
+            'wse.domain.foreign.study.WordStudyUseCase._create_task'
+        ) as mock_create_task:
+            use_case.start()
+            mock_create_task.assert_called_once_with()
+            mock_domain.start.assert_called_once_with()
