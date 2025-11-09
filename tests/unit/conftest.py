26c513e813f642f294d1f5a5889abff7f81f4dd5
Sergei Mikurov <454004@mail.ru>
2025-11-10 00:05:21 +0500

    test(conftest): Add fixtures
    - Added `word_data` fixture for word study data
    - Added `word_case` fixture for word study case

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -1,12 +1,14 @@
 """Test configuration."""
 
 import pytest
+import uuid
 
 from unittest.mock import Mock
 
 from wse.core.navigation.navigator import Navigator
 from wse.feature import observer
 from wse.feature.observer.subject import Subject
+from wse.data.sources.foreign import schemas
 
 
 @pytest.fixture
@@ -31,3 +33,24 @@ def mock_subject() -> Mock:
 def mock_observer() -> Mock:
     """Mock container content fixture."""
     return Mock()
+
+
+# Data fixtures
+# -------------
+
+@pytest.fixture
+def word_data() -> schemas.WordPresentationSchema:
+    """Word data fixture."""
+    return schemas.WordPresentationSchema(definition='test', explanation='тест')
+
+
+@pytest.fixture
+def word_case(
+    word_data: schemas.WordPresentationSchema,
+) -> schemas.WordPresentationSchema:
+    """Word study case fixture."""
+    return schemas.WordStudyCaseSchema(
+        case_uuid=uuid.uuid4(),
+        definition=word_data.definition,
+        explanation=word_data.explanation,
+    )
