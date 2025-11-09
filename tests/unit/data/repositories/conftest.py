ce490fa0ca0e82485879aaba0278ca5c2569431f
Sergei Mikurov <454004@mail.ru>
2025-11-10 01:47:28 +0500

    test(unit/repo): Add fixture
    - Added `mock_word_progress_source` fixture for
      `WordStudyProgressNetworkSourceABC`

--- tests/unit/data/repositories/conftest.py
+++ tests/unit/data/repositories/conftest.py
@@ -4,29 +4,55 @@ from unittest.mock import Mock
 
 import pytest
 
-from wse.data.repositories.foreign import study as repos
+from wse.data.repositories.foreign import study, progress
 from wse.data.sources import foreign as sources
 
 
+# Source fixtures
+# ---------------
+
+
 @pytest.fixture
-def mock_locale_source() -> Mock:
+def mock_word_locale_source() -> Mock:
     """Mock Word study Locale source."""
     return Mock(spec=sources.WordStudyLocaleSourceABC)
 
 
 @pytest.fixture
-def mock_network_source() -> Mock:
+def mock_word_network_source() -> Mock:
     """Mock Word study Network source."""
     return Mock(spec=sources.WordStudyNetworkSourceABC)
 
 
 @pytest.fixture
+def mock_word_progress_source() -> Mock:
+    """Mock the Word study progress source."""
+    return Mock(spec=sources.WordStudyProgressNetworkSourceABC)
+
+
+# Repository fixtures
+# -------------------
+
+
+@pytest.fixture
 def word_study_repo(
-    mock_locale_source: Mock,
-    mock_network_source: Mock,
-) -> repos.WordStudyRepo:
-    """Word study repo fixture."""
-    return repos.WordStudyRepo(
-        _locale_source=mock_locale_source,
-        _network_source=mock_network_source,
+    mock_word_locale_source: Mock,
+    mock_word_network_source: Mock,
+) -> study.WordStudyRepo:
+    """Word study repository."""
+    return study.WordStudyRepo(
+        _locale_source=mock_word_locale_source,
+        _network_source=mock_word_network_source,
+    )
+
+
+@pytest.fixture
+def word_study_progress_repo(
+    mock_word_locale_source: sources.WordStudyLocaleSourceABC,
+    mock_word_progress_source: sources.WordStudyProgressNetworkSourceABC,
+) -> progress.WordStudyProgressRepo:
+    """Word study progress repository fixture."""
+    return progress.WordStudyProgressRepo(
+        case_source=mock_word_locale_source,
+        progress_source=mock_word_progress_source,
     )
