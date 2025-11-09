929fc884ab7763a3cb004c0f498b957dd0175038
Sergei Mikurov <454004@mail.ru>
2025-11-10 01:49:30 +0500

    test(unit/repo): Add word study progress test

--- tests/unit/data/repositories/test_study.py
+++ tests/unit/data/repositories/test_study.py
@@ -1,9 +1,49 @@
 """Word study repository tests."""
 
+import uuid
 from unittest.mock import Mock
 
-from wse.data.repositories.foreign import study as repos
+from wse.data.repositories.foreign import progress as progress_repos
+from wse.data.repositories.foreign import study as study_repos
 from wse.data.sources.foreign import schemas
+
+
+class TestWordStudyProgress:
+    """Test Word study progress repository."""
+
+    def test_increment_progress(
+        self,
+        mock_word_locale_source: Mock,
+        mock_word_progress_source: Mock,
+        word_study_progress_repo: progress_repos.WordStudyProgressRepo,
+    ) -> None:
+        """Test the increment progress."""
+        case_uuid = uuid.uuid4()
+        mock_word_locale_source.get_case_uuid.return_value = case_uuid
+
+        word_study_progress_repo.increment()
+
+        mock_word_locale_source.get_case_uuid.assert_called_once_with()
+        mock_word_progress_source.increment_progress.assert_called_once_with(
+            case_uuid
+        )
+
+    def test_decrement_progress(
+        self,
+        mock_word_locale_source: Mock,
+        mock_word_progress_source: Mock,
+        word_study_progress_repo: progress_repos.WordStudyProgressRepo,
+    ) -> None:
+        """Test the increment progress."""
+        case_uuid = uuid.uuid4()
+        mock_word_locale_source.get_case_uuid.return_value = case_uuid
+
+        word_study_progress_repo.decrement()
+
+        mock_word_locale_source.get_case_uuid.assert_called_once_with()
+        mock_word_progress_source.decrement_progress.assert_called_once_with(
+            case_uuid
+        )
 
 
 class TestWordStudyRepo:
@@ -11,19 +51,19 @@ class TestWordStudyRepo:
 
     def test_get_word(
         self,
-        mock_locale_source: Mock,
-        mock_network_source: Mock,
+        mock_word_locale_source: Mock,
+        mock_word_network_source: Mock,
         word_case: schemas.WordStudyCaseSchema,
         word_data: schemas.WordPresentationSchema,
-        word_study_repo: repos.WordStudyRepo,
+        word_study_repo: study_repos.WordStudyRepo,
     ) -> None:
         """Test the get word to study."""
-        mock_network_source.fetch_presentation.return_value = word_case
-        mock_locale_source.get_presentation_data.return_value = word_data
+        mock_word_network_source.fetch_presentation.return_value = word_case
+        mock_word_locale_source.get_presentation_data.return_value = word_data
 
         data = word_study_repo.get_word()
 
         assert data == word_data
-        mock_network_source.fetch_presentation.assert_called_once_with()
-        mock_locale_source.set_case.assert_called_once_with(word_case)
-        mock_locale_source.get_presentation_data.assert_called_once_with()
+        mock_word_network_source.fetch_presentation.assert_called_once_with()
+        mock_word_locale_source.set_case.assert_called_once_with(word_case)
+        mock_word_locale_source.get_presentation_data.assert_called_once_with()
