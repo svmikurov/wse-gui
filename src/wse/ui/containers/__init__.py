180e97365a7d1d2bfebf3988ca56e2815170927e
Sergei Mikurov <454004@mail.ru>
2025-11-15 02:07:58 +0500

    refactor(ui/containers): Update imports

--- src/wse/ui/containers/__init__.py
+++ src/wse/ui/containers/__init__.py
@@ -1,1 +1,11 @@
 """UI containers."""
+
+__all__ = [
+    'PresenterContainer',
+    'InfoContainer',
+    'ControlContainer',
+]
+
+from .control.container import ControlContainer
+from .info.container import InfoContainer
+from .presentation.presenter.container import PresenterContainer
