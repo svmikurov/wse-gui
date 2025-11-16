855c860e78a9cbb0ae1c412b304bf2a55a74110d
Sergei Mikurov <454004@mail.ru>
2025-11-17 00:31:33 +0500

    test(unit/api): Add fetch presentation case test

---
+++ tests/unit/api/foreign/presentation/cases.py
@@ -0,0 +1,60 @@
+"""Presentation api test cases."""
+
+import uuid
+from typing import Literal, TypedDict
+
+# Case type definition
+# --------------------
+
+
+class RelatedDict(TypedDict):
+    """Response related data typed dict."""
+
+    balance: str
+
+
+class PresentationInfoDict(TypedDict):
+    """Presentation case info typed dict."""
+
+    progress: int
+
+
+class PresentationCaseDict(TypedDict):
+    """Presentation case typed dict."""
+
+    case_uuid: uuid.UUID
+    definition: str
+    explanation: str
+    info: PresentationInfoDict
+
+
+class ResponseDict(TypedDict):
+    """Presentation response typed dict."""
+
+    status: Literal['success', 'error']
+    code: int
+    message: str | None
+    related_data: RelatedDict
+    data: PresentationCaseDict
+
+
+# Cases
+# -----
+
+
+VALID_RESPONSE_PAYLOAD = ResponseDict(
+    status='success',
+    message='Some message',
+    code=200,
+    related_data=RelatedDict(
+        balance='12',
+    ),
+    data=PresentationCaseDict(
+        case_uuid=uuid.UUID('5b518a3e-45a4-4147-a097-0ed28211d8a4'),
+        definition='test',
+        explanation='тест',
+        info=PresentationInfoDict(
+            progress=8,
+        ),
+    ),
+)
---
+++ tests/unit/api/foreign/presentation/test_presentation.py
@@ -0,0 +1,97 @@
+"""Word study Presentation test."""
+
+from unittest.mock import Mock
+
+import pytest
+
+from wse.api.foreign.study import WordStudyPresentationApi
+from wse.config.api import APIConfigV1
+from wse.core.http import AuthSchemaABC, HttpClientABC
+from wse.data.sources.foreign import schemas
+
+from . import cases
+
+# Request/Response data fixtures
+# ------------------------------
+
+
+@pytest.fixture
+def request_payload() -> schemas.PresentationParams:
+    """Provide request payload via Presentation params."""
+    return schemas.PresentationParams(
+        category=None,
+        label=None,
+    )
+
+
+@pytest.fixture
+def response_payload() -> cases.ResponseDict:
+    """Provide Presentation case response fixture."""
+    return cases.VALID_RESPONSE_PAYLOAD
+
+
+# Mock api client dependency fixtures
+# -----------------------------------
+
+
+@pytest.fixture
+def mock_http_client(
+    response_payload: cases.ResponseDict,
+) -> Mock:
+    """Mock http client."""
+    mock_client = Mock(spec=HttpClientABC)
+
+    # Mock `json()` method return data
+    payload = Mock()
+    payload.json.return_value = response_payload
+
+    # Mock `post()` method return response
+    mock_client.post.return_value = payload
+    return mock_client
+
+
+@pytest.fixture
+def mock_auth_scheme() -> Mock:
+    """Mock Authentication scheme."""
+    mock = Mock(spec=AuthSchemaABC)
+    return mock
+
+
+@pytest.fixture
+def mock_api_config() -> Mock:
+    """Mock Authentication scheme."""
+    mock = Mock(spec=APIConfigV1)
+    mock.word_presentation = 'url_path'
+    return mock
+
+
+# Api client fixture with mocked dependencies
+# -------------------------------------------
+
+
+@pytest.fixture
+def api_deps_mock(
+    mock_http_client: Mock,
+    mock_auth_scheme: Mock,
+    mock_api_config: Mock,
+) -> WordStudyPresentationApi:
+    """Provide api client with mocked dependency."""
+    return WordStudyPresentationApi(
+        http_client=mock_http_client,
+        auth_scheme=mock_auth_scheme,
+        api_config=mock_api_config,
+    )
+
+
+class TestResponse:
+    """Test Presentation case."""
+
+    def test_presentation_case_validation(
+        self,
+        request_payload: schemas.PresentationParams,
+        api_deps_mock: WordStudyPresentationApi,
+    ) -> None:
+        """Test the Presentation case response validation."""
+        # Act & Assert
+        # Response payload success validated via `pydantic` schema
+        assert api_deps_mock.fetch_presentation(request_payload)
