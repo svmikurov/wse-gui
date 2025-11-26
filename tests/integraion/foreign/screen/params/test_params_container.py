82cff19b647c70ff87ad19b00eb0110573ce5b09
Sergei Mikurov <454004@mail.ru>
2025-11-26 12:17:43 +0500

    test(integration/foreign): Add a missed test
    - Added tests of selection option value for word study parameters

--- tests/integraion/foreign/screen/params/test_params_container.py
+++ tests/integraion/foreign/screen/params/test_params_container.py
@@ -32,7 +32,7 @@ class TestUpdateParameterWidget:
         # Updated source typing?
 
         # Assert
-        # - That selection has options
+        # - selection has options
         assert con._category.items._items == dto.categories  # type: ignore[union-attr]
         assert con._mark.items._items == dto.marks  # type: ignore[union-attr]
         assert con._word_source.items._items == dto.sources  # type: ignore[union-attr]
@@ -40,13 +40,15 @@ class TestUpdateParameterWidget:
         assert con._start_period.items._items == dto.periods  # type: ignore[union-attr]
         assert con._end_period.items._items == dto.periods  # type: ignore[union-attr]
 
-        # TODO: Add option value assertions
-
-        # - That selection has option value
+        # - selection has option value
         assert con._category.value == dto.category
-        ...
+        assert con._mark.value == dto.mark
+        assert con._word_source.value == dto.word_source
+        assert con._translation_order.value == dto.translation_order
+        assert con._start_period.value == dto.start_period
+        assert con._end_period.value == dto.end_period
 
-        # - That number input has value
+        # - number input has value
         assert int(con._word_count.value) == dto.word_count  # type: ignore[arg-type]
         assert int(con._question_timeout.value) == dto.question_timeout  # type: ignore[arg-type]
         assert int(con._answer_timeout.value) == dto.answer_timeout  # type: ignore[arg-type]
