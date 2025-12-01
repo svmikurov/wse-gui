4ed9d4a92077c15c833fb68d1702467a0fccfb9e
Sergei Mikurov <454004@mail.ru>
2025-12-01 21:19:16 +0500

    refactor(data/sources): Update word presentation repository

--- src/wse/data/repos/foreign/study.py
+++ src/wse/data/repos/foreign/study.py
@@ -1,36 +1,32 @@
-"""Foreign word study repository."""
+"""Word study Presentation repository."""
+
+from __future__ import annotations
 
 from dataclasses import dataclass
-from typing import override
+from typing import TYPE_CHECKING, override
 
 from injector import inject
 
-from wse.api.foreign import requests, schemas
 from wse.data.repos import foreign as repos
-from wse.data.sources import foreign as source
+from wse.data.sources import foreign as sources
+
+if TYPE_CHECKING:
+    from wse.data.schemas import foreign as schemas
 
 
 @inject
 @dataclass
-class WordStudyRepo(repos.WordStudyRepoABC):
-    """Word study repository."""
+class WordPresentationRepo(repos.WordPresentationRepoABC):
+    """Word study Presentation repository."""
 
-    _locale_source: source.WordStudyLocaleSourceABC
-    _network_source: source.WordStudyNetworkSourceABC
-    _params_repo: repos.WordParamsRepoABC
+    _locale_source: sources.WordPresentationLocaleSourceABC
+    _network_source: sources.WordPresentationNetworkSourceABC
+    _params_repo: repos.WordParametersRepoABC
 
     @override
-    def get_word(self) -> schemas.PresentationSchema:
-        """Get word to study."""
-        params = self._get_params()
-
-        try:
-            case = self._network_source.fetch_presentation(params)
-        except Exception:
-            raise
+    def get_word(self) -> schemas.Presentation:
+        """Get Word study schema."""
+        params = self._params_repo.get()
+        case = self._network_source.fetch_presentation(params)
         self._locale_source.set_case(case)
         return self._locale_source.get_presentation_data()
-
-    def _get_params(self) -> requests.InitialParametersDTO:
-        params = self._params_repo.get_params()
-        return params
