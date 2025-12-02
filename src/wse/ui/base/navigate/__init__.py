868789df1c6546bb60ce780d27dd2f5b096a8af3
Sergei Mikurov <454004@mail.ru>
2025-12-03 01:02:19 +0500

    chore(ui/base): Add import to init module

--- src/wse/ui/base/navigate/__init__.py
+++ src/wse/ui/base/navigate/__init__.py
@@ -5,7 +5,9 @@ __all__ = [
     'NavigateABC',
     'OnCloseABC',
     'OnOpenABC',
+    'NavigateStateMixin',
 ]
 
 from .abc import NavigateABC, OnCloseABC, OnOpenABC
 from .abc_button import CreateNavButtonABC
+from .mixin import NavigateStateMixin
