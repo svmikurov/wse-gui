e5465cfad924e4ba8c28678e5636a7e6b2f4993e
Sergei Mikurov <454004@mail.ru>
2025-12-08 03:14:39 +0500

    feat(core/exceprion): Add no server data exception

--- src/wse/core/exceptions/api.py
+++ src/wse/core/exceptions/api.py
@@ -3,3 +3,7 @@
 
 class ServerNotAvailableError(Exception):
     """Raised when the server is unavailable."""
+
+
+class NoResponseDataError(Exception):
+    """Raised when the server was not return data."""
